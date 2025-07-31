8d7587f44cc10844d0a4f8ce4eff4d0e5b110a79
Teddi <teddikao@gmail.com>
2025-08-01 07:30:30 +0800

    Create basic auth function

--- frontend/src/features/auth/components/ProtectedRoute.tsx
+++ frontend/src/features/auth/components/ProtectedRoute.tsx
@@ -1,11 +1,38 @@
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
+import { ACCESS_TOKEN_KEY } from "../constants";
+import { jwtDecode } from "jwt-decode";
 
 interface ProtectedRouteProps {
     children: ReactNode;
 }
 
 function ProtectedRoute({ children }: ProtectedRouteProps) {
-    
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const auth = async () => {
+        const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY)
+        if (!accessToken) {
+            setIsAuthenticated(false);
+            return;
+        }
+
+        const decodedToken = jwtDecode(accessToken);
+        const tokenExpiration = decodedToken.exp
+
+        if (!tokenExpiration) {
+            setIsAuthenticated(false);
+            return;
+        }
+
+        const now = Date.now() / 1000
+
+        const isExpired = tokenExpiration >= now
+        if (!isExpired) {
+
+        } else {
+            setIsAuthenticated(true);
+        }
+    }
 }
 
 export default ProtectedRoute;
