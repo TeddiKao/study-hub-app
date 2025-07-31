17f5c4785cc5be98bd842eb1211582761eadfb68
Teddi <teddikao@gmail.com>
2025-07-31 15:42:36 +0800

    Add endpoint for creating user

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,1 +1,13 @@
 from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView
+
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer
+
+class CreateUserEndpoint(CreateAPIView):
+    user_model = get_user_model()
+
+    queryset = user_model.objects.all()
+    serializer_class = UserSerializer()
+    permission_classes = [AllowAny]
