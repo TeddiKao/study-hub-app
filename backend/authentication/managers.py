3695d3f17bbe22e5689e37df244602961da12409
Teddi <teddikao@gmail.com>
2025-07-31 15:28:12 +0800

    Create custom user manager

--- backend/authentication/managers.py
+++ backend/authentication/managers.py
@@ -1,2 +1,16 @@
-class AuthUserManager():
-    pass
+from django.contrib.auth.models import BaseUserManager
+
+class AuthUserManager(BaseUserManager):
+    def create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError("Email must be set!")
+        
+        if not username:
+            raise ValueError("Username must be set!")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
