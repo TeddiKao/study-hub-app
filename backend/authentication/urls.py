26169bbdcd30d943db1626b98adb87cb62693b6d
Teddi <teddikao@gmail.com>
2025-07-31 15:48:02 +0800

    Add URL configurations to auth app URL file

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,5 +1,12 @@
+from django.urls import path
+
 from rest_framework_simplejwt.views import TokenObtainPairView as TokenObtainPairEndpoint
 from rest_framework_simplejwt.views import TokenRefreshView as TokenRefreshEndpoint
 
 from .views import CreateUserEndpoint
 
+urlpatterns = [
+    path("token/get/", TokenObtainPairEndpoint.as_view(), name="get_token_pair"),
+    path("token/refresh/", TokenRefreshEndpoint.as_view(), name="refresh_token"),
+    path("create-user/", CreateUserEndpoint.as_view(), name="create_user")
+]
