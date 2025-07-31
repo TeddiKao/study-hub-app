b4b8455aa7a5f02e9717839eca7c058099a11727
Teddi <teddikao@gmail.com>
2025-07-31 20:45:06 +0800

    Add import path resolutions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@app": path.resolve(__dirname, "./src/app"),
+			"@appApi": path.resolve(__dirname, "./src/app/api"),
+			"@auth": path.resolve(__dirname, "./src/features/auth")
+		}
+	}
 });
