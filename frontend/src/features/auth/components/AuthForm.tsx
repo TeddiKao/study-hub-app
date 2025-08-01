24dc517ba6b710f4beb558c094457c8279040cb4
Teddi <teddikao@gmail.com>
2025-08-01 20:35:21 +0800

    Add remaining basic markup for AuthForm component

--- frontend/src/features/auth/components/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm.tsx
@@ -1,13 +1,25 @@
 interface AuthFormProps {
-    authMethod: "Login" | "Sign up"
+	authMethod: "Login" | "Sign up";
 }
 
 function AuthForm({ authMethod }: AuthFormProps) {
-    return (
-        <div className="rounded-sm bg-white flex flex-col">
-            <h1 className="text-4xl text-center">{authMethod}</h1>
-        </div>
-    )
+	return (
+		<div className="rounded-sm bg-white flex flex-col">
+			<h1 className="text-4xl text-center">{authMethod}</h1>
+
+			<input
+				className="bg-gray-50 pl-2 pt-2 pb-2"
+				type="text"
+				placeholder="Email"
+			/>
+
+			<input
+				className="bg-gray-50 pl-2 pt-2 pb-2"
+				type="text"
+				placeholder="Password"
+			/>
+		</div>
+	);
 }
 
 export default AuthForm;
