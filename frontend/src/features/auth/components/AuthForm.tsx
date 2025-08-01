390c53d420ddc6a87f154356737d4f71fbddc497
Teddi <teddikao@gmail.com>
2025-08-01 20:14:00 +0800

    Add props interface for AuthForm component

--- frontend/src/features/auth/components/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm.tsx
@@ -1,5 +1,13 @@
-function AuthForm() {
+interface AuthFormProps {
+    authMethod: "Login" | "Sign up"
+}
 
+function AuthForm({ authMethod }: AuthFormProps) {
+    return (
+        <div className="">
+
+        </div>
+    )
 }
 
 export default AuthForm;
