81e76ae1a7a5cde8b363587378d53048762697c6
Teddi <teddikao@gmail.com>
2025-08-01 20:39:37 +0800

    Add routing configuration for login and signup pages

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,13 @@
+import LoginPage from "@/features/auth/pages/LoginPage";
+import SignupPage from "@/features/auth/pages/SignupPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/signup" element={<SignupPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
