abb95bf802ffb983969dca639fb8205d4b414f26
Teddi <teddikao@gmail.com>
2025-08-01 20:24:42 +0800

    Add styles for heading

--- frontend/src/features/auth/components/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm.tsx
@@ -5,7 +5,7 @@ interface AuthFormProps {
 function AuthForm({ authMethod }: AuthFormProps) {
     return (
         <div className="rounded-sm bg-white">
-
+            <h1 className="text-4xl text-center">{authMethod}</h1>
         </div>
     )
 }
