ed93689f809a3bb2a503ed4dff2835d4d7fe6b06
Teddi <teddikao@gmail.com>
2025-08-02 11:23:17 +0800

    Create interface for auth credentials store

--- frontend/src/features/auth/stores/authForm.stores.ts
+++ frontend/src/features/auth/stores/authForm.stores.ts
@@ -1,6 +1,16 @@
 import { create } from "zustand"
 
-const useAuthCredentialsStore = create((set) => ({
+interface AuthCredentialsStore {
+    email: string;
+    username: string;
+    password: string;
+
+    updateEmail: (newEmail: string) => void;
+    updateUsername: (newUsername: string) => void;
+    updatePassword: (newPassword: string) => void;
+}
+
+const useAuthCredentialsStore = create<AuthCredentialsStore>((set) => ({
     email: "",
     username: "",
     password: "",
