045232a9c6c5222ee40dda69653ee47fd60a324a
Teddi <teddikao@gmail.com>
2025-08-02 08:47:16 +0800

    Add styles to position AuthForm in centre of page

--- frontend/src/features/auth/components/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm.tsx
@@ -4,20 +4,24 @@ interface AuthFormProps {
 
 function AuthForm({ authMethod }: AuthFormProps) {
 	return (
-		<div className="rounded-sm bg-white flex flex-col">
-			<h1 className="text-4xl text-center font-semibold">{authMethod}</h1>
+		<div className="flex flex-col items-center justify-center h-full">
+			<div className="rounded-sm bg-white flex flex-col w-full max-w-md mx-auto">
+				<h1 className="text-4xl text-center font-semibold">
+					{authMethod}
+				</h1>
 
-			<input
-				className="bg-gray-50 pl-2 pt-2 pb-2"
-				type="text"
-				placeholder="Email"
-			/>
+				<input
+					className="bg-gray-50 pl-2 pt-2 pb-2"
+					type="text"
+					placeholder="Email"
+				/>
 
-			<input
-				className="bg-gray-50 pl-2 pt-2 pb-2"
-				type="text"
-				placeholder="Password"
-			/>
+				<input
+					className="bg-gray-50 pl-2 pt-2 pb-2"
+					type="text"
+					placeholder="Password"
+				/>
+			</div>
 		</div>
 	);
 }
