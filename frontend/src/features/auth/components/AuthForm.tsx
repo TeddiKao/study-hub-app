2da003f6b83bb37da7b8a8c14f35a98d398bf254
Teddi <teddikao@gmail.com>
2025-08-02 09:17:55 +0800

    Add hover styles for button

--- frontend/src/features/auth/components/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm.tsx
@@ -28,7 +28,7 @@ function AuthForm({ authMethod }: AuthFormProps) {
 					/>
 				</div>
 
-				<button className="bg-sky-500 pt-2 pb-2 text-white rounded-md">{authMethod}</button>
+				<button className="bg-sky-500 pt-2 pb-2 text-white rounded-md hover:cursor-pointer hover:bg-sky-700">{authMethod}</button>
 			</form>
 		</div>
 	);
