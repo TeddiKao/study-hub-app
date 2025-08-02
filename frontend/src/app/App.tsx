55ca1ec32ace0918e557b134f71b1d40dbe93352
Teddi <teddikao@gmail.com>
2025-08-02 13:42:05 +0800

    Add logout functionality

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,18 @@
 import LoginPage from "@/features/auth/pages/LoginPage";
 import SignupPage from "@/features/auth/pages/SignupPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import "./global.css";
 import ProtectedRoute from "@/features/auth/components/ProtectedRoute";
 import DashboardPage from "@/shared/pages/DashboardPage";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/features/auth/constants";
+
+function Logout() {
+	localStorage.removeItem(ACCESS_TOKEN_KEY);
+	localStorage.removeItem(REFRESH_TOKEN_KEY);
+
+	return <Navigate to="/login" />
+}
 
 function App() {
 	return (
@@ -17,6 +25,7 @@ function App() {
 						<DashboardPage />
 					</ProtectedRoute>
 				} />
+				<Route path="/logout" element={<Logout />} />
 			</Routes>
 		</BrowserRouter>
 	);
