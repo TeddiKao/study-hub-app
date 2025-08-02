b19d0ce61cbf4e08085447e8e3caf30fa6788079
Teddi <teddikao@gmail.com>
2025-08-02 08:53:19 +0800

    Add spacing between heading and authentication form

--- frontend/src/features/auth/components/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm.tsx
@@ -6,7 +6,7 @@ function AuthForm({ authMethod }: AuthFormProps) {
 	return (
 		<div className="flex flex-col items-center justify-center h-full">
 			<div className="rounded-sm bg-white shadow- flex flex-col w-full max-w-md mx-auto pl-2 pr-2 pt-3 pb-3">
-				<h1 className="text-4xl text-center font-semibold">
+				<h1 className="text-4xl text-center font-semibold mb-3">
 					{authMethod}
 				</h1>
 
