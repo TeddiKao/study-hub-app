29f8b6baf3518869dfe243c2cba01845a64b536f
Teddi <teddikao@gmail.com>
2025-08-02 12:53:39 +0800

    Add routing configuration for DashboardPage component

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,6 +3,8 @@ import SignupPage from "@/features/auth/pages/SignupPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./global.css";
+import ProtectedRoute from "@/features/auth/components/ProtectedRoute";
+import DashboardPage from "@/shared/pages/DashboardPage";
 
 function App() {
 	return (
@@ -10,6 +12,11 @@ function App() {
 			<Routes>
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/signup" element={<SignupPage />} />
+				<Route path="/home" element={
+					<ProtectedRoute>
+						<DashboardPage />
+					</ProtectedRoute>
+				} />
 			</Routes>
 		</BrowserRouter>
 	);
