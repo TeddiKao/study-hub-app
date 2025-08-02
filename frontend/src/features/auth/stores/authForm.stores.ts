b18d136e54eca0ccf20a0afad7fdb17f25ed8a42
Teddi <teddikao@gmail.com>
2025-08-02 11:13:18 +0800

    Create store for signup credentials

--- frontend/src/features/auth/stores/authForm.stores.ts
+++ frontend/src/features/auth/stores/authForm.stores.ts
@@ -7,4 +7,14 @@ const useLoginCredentialsStore = create((set) => ({
     updatePassword: (newPassword: string) => set({ password: newPassword })
 }))
 
-export { useLoginCredentialsStore }
+const useSignupCredentialsStore = create((set) => ({
+    email: "",
+    username: "",
+    password: "",
+
+    updateEmail: (newEmail: string) => set({ email: newEmail }),
+    updateUsername: (newUsername: string) => set({ username: newUsername }),
+    updatePassword: (newPassword: string) => set({ password: newPassword })
+}))
+
+export { useLoginCredentialsStore, useSignupCredentialsStore }
