9f0cce821e5e164a055996a2d102088bc7b9afce
Teddi <teddikao@gmail.com>
2025-08-02 21:19:37 +0800

    Wrap token removal logic in useEffect

--- frontend/src/features/auth/pages/LogoutRoute.tsx
+++ frontend/src/features/auth/pages/LogoutRoute.tsx
@@ -1,11 +1,14 @@
 import { Navigate } from "react-router-dom";
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "../constants";
+import { useEffect } from "react";
 
 function LogoutRoute() {
-    localStorage.removeItem(ACCESS_TOKEN_KEY);
-    localStorage.removeItem(REFRESH_TOKEN_KEY);
+	useEffect(() => {
+		localStorage.removeItem(ACCESS_TOKEN_KEY);
+		localStorage.removeItem(REFRESH_TOKEN_KEY);
+	}, []);
 
-    return <Navigate to="/login" />
+	return <Navigate to="/login" />;
 }
 
 export default LogoutRoute;
