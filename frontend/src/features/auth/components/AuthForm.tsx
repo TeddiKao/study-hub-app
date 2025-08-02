6566d3c73d36bbf695edd2b420ffed5b6213fc30
Teddi <teddikao@gmail.com>
2025-08-02 08:52:33 +0800

    Add styling to enhance visual look of authentication form

--- frontend/src/features/auth/components/AuthForm.tsx
+++ frontend/src/features/auth/components/AuthForm.tsx
@@ -5,19 +5,19 @@ interface AuthFormProps {
 function AuthForm({ authMethod }: AuthFormProps) {
 	return (
 		<div className="flex flex-col items-center justify-center h-full">
-			<div className="rounded-sm bg-white flex flex-col w-full max-w-md mx-auto">
+			<div className="rounded-sm bg-white shadow- flex flex-col w-full max-w-md mx-auto pl-2 pr-2 pt-3 pb-3">
 				<h1 className="text-4xl text-center font-semibold">
 					{authMethod}
 				</h1>
 
 				<input
-					className="bg-gray-50 pl-2 pt-2 pb-2"
+					className="bg-gray-100 pl-2 pt-2 pb-2"
 					type="text"
 					placeholder="Email"
 				/>
 
 				<input
-					className="bg-gray-50 pl-2 pt-2 pb-2"
+					className="bg-gray-100 pl-2 pt-2 pb-2"
 					type="text"
 					placeholder="Password"
 				/>
