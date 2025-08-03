2a7fbf2925024c935f768d6cc9baf910b74dd05d
Teddi <teddikao@gmail.com>
2025-08-03 18:34:11 +0800

    Create utility function for formatting error messages

--- frontend/src/features/auth/utils/authErrors.ts
+++ frontend/src/features/auth/utils/authErrors.ts
@@ -1,5 +1,7 @@
-function formatErrorMessage() {
+import { authFieldErrorsMap } from "../constants/authErrors.constants";
 
+function formatErrorMessage(rawErrorMessage: string) {
+	return authFieldErrorsMap[rawErrorMessage];
 }
 
 export default formatErrorMessage;
