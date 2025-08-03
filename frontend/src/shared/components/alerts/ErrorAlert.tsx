768fd7b51e4178446bb552a825fb05b1adc5867f
Teddi <teddikao@gmail.com>
2025-08-03 19:38:49 +0800

    Add basic markup and display logic for ErrorAlert component

--- frontend/src/shared/components/alerts/ErrorAlert.tsx
+++ frontend/src/shared/components/alerts/ErrorAlert.tsx
@@ -1,5 +1,19 @@
-function ErrorAlert() {
+import type { ReactNode } from "react";
 
+interface ErrorAlertProps {
+    visible: boolean;
+    alertContent: string;
+    children: ReactNode;
+}
+
+function ErrorAlert({ alertContent, visible, children }: ErrorAlertProps) {
+    if (!visible) return null;
+
+    return (
+        <div className="">
+            {children}
+        </div>
+    )
 }
 
 export default ErrorAlert;
