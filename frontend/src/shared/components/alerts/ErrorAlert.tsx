3ebc6c3f16929388283ecb14aa1e392cb28daea8
Teddi <teddikao@gmail.com>
2025-08-03 19:43:42 +0800

    Enhance markup for ErrorAlert component

--- frontend/src/shared/components/alerts/ErrorAlert.tsx
+++ frontend/src/shared/components/alerts/ErrorAlert.tsx
@@ -1,19 +1,19 @@
-import type { ReactNode } from "react";
+import errorIcon from "../../assets/errorIcon.svg";
 
 interface ErrorAlertProps {
-    visible: boolean;
-    alertContent: string;
-    children: ReactNode;
+	visible: boolean;
+	alertContent: string;
 }
 
-function ErrorAlert({ alertContent, visible, children }: ErrorAlertProps) {
-    if (!visible) return null;
+function ErrorAlert({ alertContent, visible }: ErrorAlertProps) {
+	if (!visible) return null;
 
-    return (
-        <div className="">
-            {children}
-        </div>
-    )
+	return (
+		<div className="flex flex-row fixed bottom-2 right-2 rounded-md bg-red-500">
+			<img src={errorIcon} alt="Error icon" />
+            <p className="text-white ml-1">{alertContent}</p>
+		</div>
+	);
 }
 
 export default ErrorAlert;
