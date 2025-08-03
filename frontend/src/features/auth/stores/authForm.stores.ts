a2a93d5e307419004cc528e4c0825c8f84497586
Teddi <teddikao@gmail.com>
2025-08-03 17:30:55 +0800

    Create methods for manipulating variables in AuthErrorStore

--- frontend/src/features/auth/stores/authForm.stores.ts
+++ frontend/src/features/auth/stores/authForm.stores.ts
@@ -1,49 +1,76 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 interface AuthCredentialsStore {
-    email: string;
-    username: string;
-    password: string;
+	email: string;
+	username: string;
+	password: string;
 
-    updateEmail: (newEmail: string) => void;
-    updateUsername: (newUsername: string) => void;
-    updatePassword: (newPassword: string) => void;
+	updateEmail: (newEmail: string) => void;
+	updateUsername: (newUsername: string) => void;
+	updatePassword: (newPassword: string) => void;
 
-    clearPassword: () => void;
-    clearAllFields: () => void;
+	clearPassword: () => void;
+	clearAllFields: () => void;
 }
 
 interface AuthErrorsStore {
-    general: string[];
-    fields: {
-        email: string[];
-        username: string[];
-        password: string[];
-    }
+	general: string[];
+	fields: {
+		email: string[];
+		username: string[];
+		password: string[];
+	};
 }
 
 const useAuthCredentialsStore = create<AuthCredentialsStore>((set) => ({
-    email: "",
-    username: "",
-    password: "",
+	email: "",
+	username: "",
+	password: "",
 
-    updateEmail: (newEmail: string) => set({ email: newEmail }),
-    updateUsername: (newUsername: string) => set({ username: newUsername }),
-    updatePassword: (newPassword: string) => set({ password: newPassword }),
+	updateEmail: (newEmail: string) => set({ email: newEmail }),
+	updateUsername: (newUsername: string) => set({ username: newUsername }),
+	updatePassword: (newPassword: string) => set({ password: newPassword }),
 
-    clearPassword: () => set({ password: "" }),
-    clearAllFields: () => set({ username: "", email: "", password: "" })
-}))
+	clearPassword: () => set({ password: "" }),
+	clearAllFields: () => set({ username: "", email: "", password: "" }),
+}));
 
 const useAuthErrorsStore = create<AuthErrorsStore>((set) => ({
-    general: [],
-    fields: {
-        email: [],
-        username: [],
-        password: [],
-    },
+	general: [],
+	fields: {
+		email: [],
+		username: [],
+		password: [],
+	},
 
-    
-}))
+	updateGeneralErrors: (generalErrors: string[]) =>
+		set({ general: generalErrors }),
+	updateEmailErrors: (emailErrors: string[]) =>
+		set((state) => ({
+			...state,
+			fields: {
+				...state.fields,
+				email: emailErrors
+			}
+		})),
 
-export { useAuthCredentialsStore }
+	updateUsernameErrors: (usernameErrors: string[]) =>
+		set((state) => ({
+			...state,
+			fields: {
+				...state.fields,
+				username: usernameErrors
+			}
+		})),
+
+	updatePasswordErrors: (passwordErrors: string[]) =>
+		set((state) => ({
+			...state,
+			fields: {
+				...state.fields,
+				password: passwordErrors
+			}
+		}))
+}));
+
+export { useAuthCredentialsStore, useAuthErrorsStore };
