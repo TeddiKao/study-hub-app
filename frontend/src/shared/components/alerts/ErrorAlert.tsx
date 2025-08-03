69c41539aa4d23da4f104311f9027d5c9c075db8
Teddi <teddikao@gmail.com>
2025-08-03 19:58:39 +0800

    Further enhance ErrorAlert design

--- frontend/src/shared/components/alerts/ErrorAlert.tsx
+++ frontend/src/shared/components/alerts/ErrorAlert.tsx
@@ -9,9 +9,12 @@ function ErrorAlert({ alertContent, visible }: ErrorAlertProps) {
 	if (!visible) return null;
 
 	return (
-		<div className="flex flex-row fixed bottom-2 right-2 rounded-md bg-red-500">
+		<div className="flex flex-row items-center fixed bottom-6 right-6 rounded-xl bg-red-500 w-max pl-2 pr-3 pt-3 pb-3">
 			<img src={errorIcon} alt="Error icon" />
-            <p className="text-white ml-1">{alertContent}</p>
+			<p className="text-white ml-1">{alertContent}</p>
+			<button className="text-white ml-8 pl-2 pr-2 pt-1 pb-1 rounded-md bg-red-300 hover:cursor-pointer hover:bg-red-400">
+				Close
+			</button>
 		</div>
 	);
 }
