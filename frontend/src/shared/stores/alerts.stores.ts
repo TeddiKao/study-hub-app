6613ace23d3f98ac19287fcf5b2fef6aef47d6da
Teddi <teddikao@gmail.com>
2025-08-04 15:40:51 +0800

    Add method for showing alert in alert store

--- frontend/src/shared/stores/alerts.stores.ts
+++ frontend/src/shared/stores/alerts.stores.ts
@@ -3,11 +3,13 @@ import { create } from "zustand";
 interface AlertVisibleStore {
     visible: boolean;
     closeAlert: () => void;
+    showAlert: () => void;
 }
 
 const useAlertVisibleStore = create<AlertVisibleStore>((set) => ({
     visible: false,
-    closeAlert: () => set({ visible: false })
+    closeAlert: () => set({ visible: false }),
+    showAlert: () => set({ visible: true })
 }))
 
 export { useAlertVisibleStore }
