1f848db5857d7b8ba9351318381700ebf78cd1ca
Teddi <teddikao@gmail.com>
2025-08-04 16:14:17 +0800

    Avoid using indices as keys in ErrorAlert component

--- frontend/src/shared/components/alerts/ErrorAlert.tsx
+++ frontend/src/shared/components/alerts/ErrorAlert.tsx
@@ -16,7 +16,7 @@ function ErrorAlert({ errorSummary, errors, visible, onClose }: ErrorAlertProps)
 			<div className="flex flex-col ml-2">
 				<p className="text-white">{errorSummary}</p>
 				<ul className="relative pl-4 text-white before:absolute before:left-0 before:top-2 before:h-1.5 before:w-1.5 before:rounded-full before:bg-white">
-					{errors.map((error, index) => <li className="text-white" key={index}>{error}</li>)}
+					{errors.map((error) => <li className="text-white" key={error}>{error}</li>)}
 				</ul>
 			</div>
 			<button onClick={onClose} className="text-white ml-8 pl-2 pr-2 pt-1 pb-1 rounded-md bg-red-300 hover:cursor-pointer hover:bg-red-400">
