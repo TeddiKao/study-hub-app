6b2bbe1e10cb57c2ee3bdbba5d14eb29193bca23
Teddi <teddikao@gmail.com>
2025-08-04 21:01:16 +0800

    Add badic positioning styling for AuthLoadingScreen

--- frontend/src/features/auth/components/AuthLoadingScreen.tsx
+++ frontend/src/features/auth/components/AuthLoadingScreen.tsx
@@ -4,6 +4,12 @@ interface AuthLoadingScreenProps {
 
 function AuthLoadingScreen({ visible }: AuthLoadingScreenProps) {
     if (!visible) return null;
+
+    return (
+        <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white">
+
+        </div>
+    )
 }
 
 export default AuthLoadingScreen;
