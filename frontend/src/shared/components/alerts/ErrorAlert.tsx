6c3f27fa325c512d350e7a55f2d46a98e9c598ec
Teddi <teddikao@gmail.com>
2025-08-04 12:40:42 +0800

    Add alert closing functionality

--- frontend/src/shared/components/alerts/ErrorAlert.tsx
+++ frontend/src/shared/components/alerts/ErrorAlert.tsx
@@ -3,16 +3,17 @@ import errorIcon from "../../assets/errorIcon.svg";
 interface ErrorAlertProps {
 	visible: boolean;
 	alertContent: string;
+    onClose: () => void;
 }
 
-function ErrorAlert({ alertContent, visible }: ErrorAlertProps) {
+function ErrorAlert({ alertContent, visible, onClose }: ErrorAlertProps) {
 	if (!visible) return null;
 
 	return (
 		<div className="flex flex-row items-center fixed bottom-6 right-6 rounded-xl bg-red-500 w-max pl-2 pr-3 pt-3 pb-3">
 			<img src={errorIcon} alt="Error icon" />
 			<p className="text-white ml-1">{alertContent}</p>
-			<button className="text-white ml-8 pl-2 pr-2 pt-1 pb-1 rounded-md bg-red-300 hover:cursor-pointer hover:bg-red-400">
+			<button onClick={onClose} className="text-white ml-8 pl-2 pr-2 pt-1 pb-1 rounded-md bg-red-300 hover:cursor-pointer hover:bg-red-400">
 				Close
 			</button>
 		</div>
