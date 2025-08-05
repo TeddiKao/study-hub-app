ceea36a05485ce98c9202ade1d2061dfd746faaf
Teddi <teddikao@gmail.com>
2025-08-05 12:35:36 +0800

    Add heading to auth loading screen

--- frontend/src/features/auth/components/AuthLoadingScreen.tsx
+++ frontend/src/features/auth/components/AuthLoadingScreen.tsx
@@ -1,15 +1,17 @@
 interface AuthLoadingScreenProps {
-    visible: boolean;
+	visible: boolean;
 }
 
 function AuthLoadingScreen({ visible }: AuthLoadingScreenProps) {
-    if (!visible) return null;
+	if (!visible) return null;
 
-    return (
-        <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white">
-
-        </div>
-    )
+	return (
+		<>
+			<div className="flex flex-col top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white">
+                <h3 className="text-xl font-semibold">Authenticating user</h3>
+            </div>
+		</>
+	);
 }
 
 export default AuthLoadingScreen;
