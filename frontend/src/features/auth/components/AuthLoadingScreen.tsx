afc4201e2167826fb31276ecbc7c2a4c96099600
Teddi <teddikao@gmail.com>
2025-08-05 12:40:15 +0800

    Add dynamic message generation based on auth method

--- frontend/src/features/auth/components/AuthLoadingScreen.tsx
+++ frontend/src/features/auth/components/AuthLoadingScreen.tsx
@@ -1,15 +1,21 @@
+import { loadingScreenActionMap } from "../constants/authLoadingScreen.constants";
+import type { AuthMethods } from "../types/auth.types";
+
 interface AuthLoadingScreenProps {
 	visible: boolean;
+	authMethod: AuthMethods;
 }
 
-function AuthLoadingScreen({ visible }: AuthLoadingScreenProps) {
+function AuthLoadingScreen({ visible, authMethod }: AuthLoadingScreenProps) {
 	if (!visible) return null;
 
 	return (
 		<>
 			<div className="flex flex-col top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white">
-                <h3 className="text-xl font-semibold">Authenticating user</h3>
-            </div>
+				<h3 className="text-xl font-semibold">
+					{loadingScreenActionMap[authMethod]}
+				</h3>
+			</div>
 		</>
 	);
 }
