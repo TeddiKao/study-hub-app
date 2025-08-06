dc03de89c9e66c15666a2b9fc29977ac0db0ec53
Teddi <teddikao@gmail.com>
2025-08-06 20:51:02 +0800

    Add type safety for loading screen action map

--- frontend/src/features/auth/constants/authLoadingScreen.constants.ts
+++ frontend/src/features/auth/constants/authLoadingScreen.constants.ts
@@ -1,4 +1,6 @@
-const loadingScreenActionMap = {
+import type { AuthMethods } from "../types/auth.types"
+
+const loadingScreenActionMap: Record<AuthMethods, any> = {
     "Login": "Logging in",
     "Sign up": "Signing up"
 }
