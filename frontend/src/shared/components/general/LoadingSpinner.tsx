8420b88658b5c0256cb33b8dd71023f7f62bea59
Teddi <teddikao@gmail.com>
2025-08-06 20:48:10 +0800

    Modify role value for proper accessiblity

--- frontend/src/shared/components/general/LoadingSpinner.tsx
+++ frontend/src/shared/components/general/LoadingSpinner.tsx
@@ -19,7 +19,7 @@ function LoadingSpinner({ className = "", height, width }: LoadingSpinnerProps) 
 			strokeLinecap="round"
 			strokeLinejoin="round"
             className={cn("animate-spin", className)}
-			role="auth-status"
+			role="status"
 			aria-label="loading"
 		>
 			<path d="M21 12a9 9 0 1 1-6.219-8.56" />
