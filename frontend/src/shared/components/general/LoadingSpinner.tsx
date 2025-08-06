075217f9cef39141823f627ea8b31d15e74e0201
Teddi <teddikao@gmail.com>
2025-08-06 11:14:28 +0800

    Add proper aria attributes to LoadingSpinner component

--- frontend/src/shared/components/general/LoadingSpinner.tsx
+++ frontend/src/shared/components/general/LoadingSpinner.tsx
@@ -19,6 +19,8 @@ function LoadingSpinner({ className, height, width }: LoadingSpinnerProps) {
 			strokeLinecap="round"
 			strokeLinejoin="round"
             className={cn("animate-spin", className)}
+			role="auth-status"
+			aria-label="loading"
 		>
 			<path d="M21 12a9 9 0 1 1-6.219-8.56" />
 		</svg>
