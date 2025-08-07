a4041c117f04db861d7984c90287018dbb5655b1
Teddi <teddikao@gmail.com>
2025-08-07 20:03:24 +0800

    Create props interface for BaseModal component

--- frontend/src/shared/components/container/BaseModal.tsx
+++ frontend/src/shared/components/container/BaseModal.tsx
@@ -1,5 +1,12 @@
-function BaseModal() {
+import type { ReactNode } from "react";
 
+interface BaseModalProps {
+    visible: boolean;
+    children: ReactNode;
+}
+
+function BaseModal({ visible, children }: BaseModalProps) {
+    
 }
 
 export default BaseModal;
