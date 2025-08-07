e0220c3398f559302d141d20f20b51e7c73b6827
Teddi <teddikao@gmail.com>
2025-08-07 11:06:12 +0800

    Extract auth status fetching logic into custom hook

---
+++ frontend/src/features/auth/hooks/useIsAuthenticated.ts
@@ -0,0 +1,67 @@
+import api from "@/app/api";
+import { jwtDecode } from "jwt-decode";
+import { useState, useEffect } from "react";
+import { REFRESH_TOKEN_KEY, ACCESS_TOKEN_KEY } from "../constants/tokenKeys.constants";
+
+function useIsAuthenticated() {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(
+		null
+	);
+
+	useEffect(() => {
+		auth().catch(() => {
+			setIsAuthenticated(false);
+		});
+	}, []);
+
+	const handleTokenRefresh = async () => {
+		const refreshToken = localStorage.getItem(REFRESH_TOKEN_KEY);
+		if (!refreshToken) {
+			return;
+		}
+
+		try {
+			const response = await api.post("auth/token/refresh/", {
+				refresh: refreshToken,
+			});
+
+			localStorage.setItem(ACCESS_TOKEN_KEY, response.data.access);
+			setIsAuthenticated(true);
+		} catch (error) {
+			console.error(error);
+			setIsAuthenticated(false);
+
+			localStorage.removeItem(ACCESS_TOKEN_KEY);
+			localStorage.removeItem(REFRESH_TOKEN_KEY);
+		}
+	};
+
+	const auth = async () => {
+		const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+
+		if (!accessToken) {
+			setIsAuthenticated(false);
+			return;
+		}
+
+		const decodedToken = jwtDecode(accessToken);
+		const tokenExpiration = decodedToken.exp;
+
+		if (!tokenExpiration) {
+			setIsAuthenticated(false);
+			return;
+		}
+
+		const now = Date.now() / 1000;
+
+		const isExpired = tokenExpiration < now;
+
+		if (isExpired) {
+			await handleTokenRefresh();
+		} else {
+			setIsAuthenticated(true);
+		}
+	};
+
+    return isAuthenticated;
+}
