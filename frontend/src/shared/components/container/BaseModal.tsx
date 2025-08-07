0e84b84aebd9a400cad0dca004a69d3aee9a54d2
Teddi <teddikao@gmail.com>
2025-08-07 20:08:44 +0800

    Add modal overlay display logic in BaseModal component

--- frontend/src/shared/components/container/BaseModal.tsx
+++ frontend/src/shared/components/container/BaseModal.tsx
@@ -5,21 +5,33 @@ interface BaseModalProps {
 	visible: boolean;
 	children: ReactNode;
 	ariaLabelledBy: string;
+	includeOverlay?: boolean;
 }
 
-function BaseModal({ visible, children, ariaLabelledBy }: BaseModalProps) {
-    if (!visible) return;
+function BaseModal({
+	visible,
+	children,
+	ariaLabelledBy,
+	includeOverlay = true,
+}: BaseModalProps) {
+	if (!visible) return;
 
 	return ReactDOM.createPortal(
-		<div
-			role="dialog"
-			aria-modal="true"
-			aria-labelledby={ariaLabelledBy}
-			className="flex flex-col items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 fixed z-50 bg-white p-5 rounded-lg"
-		>
-			{children}
-		</div>,
-        document.getElementById("portal") ?? document.body
+		<>
+			<div
+				role="dialog"
+				aria-modal="true"
+				aria-labelledby={ariaLabelledBy}
+				className="flex flex-col items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 fixed z-50 bg-white p-5 rounded-lg"
+			>
+				{children}
+			</div>
+
+			{includeOverlay && (
+				<div className="fixed top-0 bottom-0 left-0 right-0 opacity-95 bg-gray-950 z-40"></div>
+			)}
+		</>,
+		document.getElementById("portal") ?? document.body
 	);
 }
 
