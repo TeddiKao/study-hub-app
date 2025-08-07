e5717de50db05c2c2d3aef4740fe75bff45118b3
Teddi <teddikao@gmail.com>
2025-08-07 20:26:56 +0800

    Add FlexCenter component styling

--- frontend/src/shared/components/wrappers/FlexCenter.tsx
+++ frontend/src/shared/components/wrappers/FlexCenter.tsx
@@ -1,5 +1,15 @@
-function FlexCenter() {
+import type { ReactNode } from "react";
 
+interface FlexCenterProps {
+    children: ReactNode;
+}
+
+function FlexCenter({ children }: FlexCenterProps) {
+    return (
+        <div className="flex flex-col items-center justify-center h-full">
+            {children}
+        </div>
+    )
 }
 
 export default FlexCenter;
