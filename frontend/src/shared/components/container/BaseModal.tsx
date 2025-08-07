11438689517fea170700b3b8c5214ddebd786c62
Teddi <teddikao@gmail.com>
2025-08-07 20:06:13 +0800

    Add display logic for BaseModal

--- frontend/src/shared/components/container/BaseModal.tsx
+++ frontend/src/shared/components/container/BaseModal.tsx
@@ -1,12 +1,26 @@
 import type { ReactNode } from "react";
+import ReactDOM from "react-dom";
 
 interface BaseModalProps {
-    visible: boolean;
-    children: ReactNode;
+	visible: boolean;
+	children: ReactNode;
+	ariaLabelledBy: string;
 }
 
-function BaseModal({ visible, children }: BaseModalProps) {
-    
+function BaseModal({ visible, children, ariaLabelledBy }: BaseModalProps) {
+    if (!visible) return;
+
+	return ReactDOM.createPortal(
+		<div
+			role="dialog"
+			aria-modal="true"
+			aria-labelledby={ariaLabelledBy}
+			className="flex flex-col items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 fixed z-50 bg-white p-5 rounded-lg"
+		>
+			{children}
+		</div>,
+        document.getElementById("portal") ?? document.body
+	);
 }
 
 export default BaseModal;
