5e064aeb312e82f48a860abf379a86583d1afbc8
Teddi <teddikao@gmail.com>
2025-08-08 18:54:48 +0800

    Add basic user info display logic by displaying placeholder profile icon

--- frontend/src/shared/pages/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage.tsx
@@ -1,6 +1,15 @@
+import userIcon from "@shared/assets/userIcon.svg";
+
 function DashboardPage() {
     return (
-        <></>
+        <>
+            <div className="flex flex-row fixed top-0 bottom-0 w-max pt-1 pb-1 pl-1 pr-3">
+                <img alt="Profile Icon" src={userIcon} />
+                <div className="flex flex-column">
+                    
+                </div>
+            </div>
+        </>
     )
 }
 
