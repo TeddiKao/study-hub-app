75c94079aea7cc459ebf93e078350d3d3ffa4189
Teddi <teddikao@gmail.com>
2025-08-08 20:15:40 +0800

    Wrap account icon in PopoverTrigger component

--- frontend/src/shared/pages/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage.tsx
@@ -1,13 +1,21 @@
+import { Popover, PopoverTrigger } from "@radix-ui/react-popover";
 import userIcon from "@shared/assets/userIcon.svg";
 
 function DashboardPage() {
 	return (
 		<>
 			<div className="flex flex-row justify-center fixed top-0 bottom-0 w-max p-2 bg-white">
-				<div className="w-8 h-8 p-1 rounded-md hover:bg-gray-300 hover:cursor-pointer">
-                    <img className="w-full h-full" alt="Profile Icon" src={userIcon} />
-                </div>
-				
+				<Popover>
+					<PopoverTrigger>
+						<div className="w-8 h-8 p-1 rounded-md hover:bg-gray-300 hover:cursor-pointer">
+							<img
+								className="w-full h-full"
+								alt="Profile Icon"
+								src={userIcon}
+							/>
+						</div>
+					</PopoverTrigger>
+				</Popover>
 			</div>
 		</>
 	);
