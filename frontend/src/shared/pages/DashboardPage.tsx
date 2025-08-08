993049cb95cacd126626b8177cd582f27f957c3d
Teddi <teddikao@gmail.com>
2025-08-08 19:09:52 +0800

    Increase padding for dashboard navbar

--- frontend/src/shared/pages/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage.tsx
@@ -1,16 +1,14 @@
 import userIcon from "@shared/assets/userIcon.svg";
 
 function DashboardPage() {
-    return (
-        <>
-            <div className="flex flex-row fixed top-0 bottom-0 w-max pt-1 pb-1 pl-1 pr-3">
-                <img className="w-8 h-8" alt="Profile Icon" src={userIcon} />
-                <div className="flex flex-column">
-                    
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="flex flex-row justify-center fixed top-0 bottom-0 w-max p-3 bg-white">
+				<img className="w-8 h-8" alt="Profile Icon" src={userIcon} />
+				<div className="flex flex-column"></div>
+			</div>
+		</>
+	);
 }
 
 export default DashboardPage;
