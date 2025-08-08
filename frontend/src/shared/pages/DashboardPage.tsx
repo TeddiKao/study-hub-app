4b1a5785f0eaafb6e66dc16e0edb8a0d0933eb2c
Teddi <teddikao@gmail.com>
2025-08-08 20:33:58 +0800

    Remove outline from Popover content

--- frontend/src/shared/pages/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage.tsx
@@ -1,4 +1,8 @@
-import { Popover, PopoverTrigger } from "@radix-ui/react-popover";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@radix-ui/react-popover";
 import userIcon from "@shared/assets/userIcon.svg";
 
 function DashboardPage() {
@@ -6,7 +10,7 @@ function DashboardPage() {
 		<>
 			<div className="flex flex-row justify-center fixed top-0 bottom-0 w-max p-2 bg-white">
 				<Popover>
-					<PopoverTrigger>
+					<PopoverTrigger asChild>
 						<div className="w-8 h-8 p-1 rounded-md hover:bg-gray-300 hover:cursor-pointer">
 							<img
 								className="w-full h-full"
@@ -15,6 +19,17 @@ function DashboardPage() {
 							/>
 						</div>
 					</PopoverTrigger>
+
+					<PopoverContent
+						side="right"
+						className="outline-none ml-2 flex flex-row rounded-md shadow-md bg-white"
+					>
+						<img
+							className="w-full h-full"
+							alt="Profile Icon"
+							src={userIcon}
+						/>
+					</PopoverContent>
 				</Popover>
 			</div>
 		</>
