48c17c9a845d96026e18c019eacb1893afc5cfae
Teddi <teddikao@gmail.com>
2025-08-08 19:40:44 +0800

    Further tweak styling of dashboard navigation bar

--- frontend/src/shared/pages/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage.tsx
@@ -3,9 +3,11 @@ import userIcon from "@shared/assets/userIcon.svg";
 function DashboardPage() {
 	return (
 		<>
-			<div className="flex flex-row justify-center fixed top-0 bottom-0 w-max p-3 bg-white">
-				<img className="w-8 h-8" alt="Profile Icon" src={userIcon} />
-				<div className="flex flex-column"></div>
+			<div className="flex flex-row justify-center fixed top-0 bottom-0 w-max p-2 bg-white">
+				<div className="w-8 h-8 p-1 rounded-md hover:bg-gray-300 hover:cursor-pointer">
+                    <img className="w-full h-full" alt="Profile Icon" src={userIcon} />
+                </div>
+				
 			</div>
 		</>
 	);
