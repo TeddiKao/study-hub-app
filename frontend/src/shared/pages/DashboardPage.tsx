a748338f670b38bdf88bcd0e32ef5caf7ae84e14
Teddi <teddikao@gmail.com>
2025-08-08 20:42:01 +0800

    Add styles to move popover down

--- frontend/src/shared/pages/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage.tsx
@@ -23,6 +23,8 @@ function DashboardPage() {
 					<PopoverContent
 						side="right"
                         sideOffset={8}
+                        align="start"
+                        alignOffset={4}
 						className="outline-none p-2 flex flex-row items-center rounded-md shadow-md bg-white"
 					>
 						<img
