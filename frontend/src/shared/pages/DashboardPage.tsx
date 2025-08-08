68eecc21c9909c2f967c2dcd520b888bfa1a3230
Teddi <teddikao@gmail.com>
2025-08-08 19:03:26 +0800

    Tweak icon sizing

--- frontend/src/shared/pages/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage.tsx
@@ -4,7 +4,7 @@ function DashboardPage() {
     return (
         <>
             <div className="flex flex-row fixed top-0 bottom-0 w-max pt-1 pb-1 pl-1 pr-3">
-                <img alt="Profile Icon" src={userIcon} />
+                <img className="w-8 h-8" alt="Profile Icon" src={userIcon} />
                 <div className="flex flex-column">
                     
                 </div>
