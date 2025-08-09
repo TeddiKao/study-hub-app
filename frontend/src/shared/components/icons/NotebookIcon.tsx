98f85fc97d5b65933aac5ae37408740e8e30431c
Teddi <teddikao@gmail.com>
2025-08-09 20:56:57 +0800

    Keep notebook icon out of accessiblity tree

--- frontend/src/shared/components/icons/NotebookIcon.tsx
+++ frontend/src/shared/components/icons/NotebookIcon.tsx
@@ -11,7 +11,8 @@ function NotebookIcon({ size, className }: NotebookIconProps) {
 			viewBox="0 0 24 24"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
-			transform="rotate(0 0 0)"
+			aria-hidden="true"
+			focusable="false"
 		>
 			<path
 				fillRule="evenodd"
