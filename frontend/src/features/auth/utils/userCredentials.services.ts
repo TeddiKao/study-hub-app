b9addef06c888055c1f5cccbd66b2b29e3cafc70
Teddi <teddikao@gmail.com>
2025-08-09 10:11:00 +0800

    Create service function to fetch user credentials

---
+++ frontend/src/features/auth/utils/userCredentials.services.ts
@@ -0,0 +1,25 @@
+import api from "@/app/api";
+import type { ApiErrorResponse } from "@/shared/types/api.types";
+
+interface UserCredentialsEndpointSuccessResponse {
+	email: string;
+	password: string;
+	success: true;
+}
+
+async function fetchUserCredentials(): Promise<
+	UserCredentialsEndpointSuccessResponse | ApiErrorResponse
+> {
+	try {
+		const response = await api.post("/auth/get-credentials");
+
+		return response.data;
+	} catch (error) {
+		return {
+			success: false,
+            error: "Failed to fetch credentials"
+		};
+	}
+}
+
+export { fetchUserCredentials };
