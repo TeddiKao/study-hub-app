4681af68e35e986ddc642af94d79cea337cfd0e9
Teddi <teddikao@gmail.com>
2025-08-09 10:03:10 +0800

    Create backend API endpoint for fetching username and email

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
 
@@ -11,3 +13,12 @@ class CreateUserEndpoint(CreateAPIView):
     queryset = user_model.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class GetCredentialsEndpoint(APIView):
+    def post(self, request):
+        user = request.data.user
+
+        return Response({
+            "username": user.username,
+            "email": user.email
+        })
