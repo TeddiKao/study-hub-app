c4c74ed2a4983c5e493329e195b002e8e11cbfef
Teddi <teddikao@gmail.com>
2025-08-09 10:44:33 +0800

    Add explicit protection for credential fetching endpoint

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,6 +15,8 @@ class CreateUserEndpoint(CreateAPIView):
     permission_classes = [AllowAny]
 
 class GetCredentialsEndpoint(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         user = request.user
 
