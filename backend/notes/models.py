1b43599ec7d8654da85162f78b8e5e1e0ba88431
Teddi <teddikao@gmail.com>
2025-08-11 14:23:01 +0800

    Add owner field to notebook model

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 class Notebook(models.Model):
     name = models.CharField(max_length=60, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
     notebook_color = models.CharField(max_length=7, null=False, blank=False, default="#b3b3b3")
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
