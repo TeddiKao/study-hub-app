30bc6ac8d9f01f7525084a5d4df2c40bc1700226
Teddi <teddikao@gmail.com>
2025-08-11 20:08:17 +0800

    FIx naming inconsistency

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CreateNotebookEndpoint, FetchNotebooksEndpoint
 
 urlpatterns = [
-    path("notebooks/get/", FetchNotebooksEndpoint.as_view(), name="fetch_notebooks"),
+    path("notebooks/", FetchNotebooksEndpoint.as_view(), name="fetch_notebooks"),
     path("notebook/create/", CreateNotebookEndpoint.as_view(), name="create_notebook")
 ]
