d580095c69f0687cc4b0cb6c955dab0d584958e1
Teddi <teddikao@gmail.com>
2025-08-11 09:48:52 +0800

    Update directory structure

--- frontend/src/shared/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/shared/pages/DashboardPage/DashboardPage.tsx
@@ -1,8 +1,7 @@
-import NavIcons from "./components/NavIcons";
-import NavPanel from "./components/NavPanel";
+import NavIcons from "./components/Navbar/components/NavIcons";
+import NavPanel from "./components/Navbar/components/NavPanel";
 
 function DashboardPage() {
-
 	return (
 		<div className="flex flex-row fixed top-0 bottom-0">
 			<NavIcons />
