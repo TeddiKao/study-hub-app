c36d9f9f72e6aba64f21f8dc1ef0c4308ad9dec0
Teddi <teddikao@gmail.com>
2025-08-11 19:45:53 +0800

    Create endpoint for fetching notebooks

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,9 +1,20 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .models import Notebook
 from .serializers import NotebookSerializer
+
+class FetchNotebooksEndpoint(ListAPIView):
+    serializer_class = NotebookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter logic not implemented yet, for future purposes
+        filter = self.request.query_params.getList("filters")
+        queryset = Notebook.objects.filter(owner=self.request.user)
+
+        return queryset
 
 class CreateNotebookEndpoint(CreateAPIView):
     queryset = Notebook.objects.all()
