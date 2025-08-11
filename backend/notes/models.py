a003a4764db5a7114cb024cf506da0125c537597
Teddi <teddikao@gmail.com>
2025-08-11 14:20:05 +0800

    Add missing max_length property to notebook model

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Notebook(models.Model):
     name = models.CharField(max_length=60, null=False, blank=False, unique=True)
     description = models.TextField(null=True, blank=True)
-    notebook_color = models.CharField(null=False, blank=False, default="#b3b3b3")
+    notebook_color = models.CharField(max_length=7, null=False, blank=False, default="#b3b3b3")
