7945eaa8619966e6846779f92e73f4f99506b0ee
Teddi <teddikao@gmail.com>
2025-08-11 17:33:49 +0800

    Add endpoint for creating a notebook

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import Notebook
+from .serializers import NotebookSerializer
 
 # Create your views here.
+class CreateNotebookEndpoint(CreateAPIView):
+    queryset = Notebook.objects.all()
+    serializer_class = NotebookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(owner=self.request.user)
+        else:
+            return Response({
+                "errors": serializer.errors
+            })
