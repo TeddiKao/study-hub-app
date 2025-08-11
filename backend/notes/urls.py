2d079957d74fcd24911560c5442c0f3397a2c4a6
Teddi <teddikao@gmail.com>
2025-08-11 20:22:07 +0800

    Add URL configuration for deleting notebook

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateNotebookEndpoint, FetchNotebooksEndpoint
+from .views import CreateNotebookEndpoint, FetchNotebooksEndpoint, DeleteNotebookEndpoint
 
 urlpatterns = [
     path("notebooks/", FetchNotebooksEndpoint.as_view(), name="fetch_notebooks"),
-    path("notebook/create/", CreateNotebookEndpoint.as_view(), name="create_notebook")
+    path("notebook/create/", CreateNotebookEndpoint.as_view(), name="create_notebook"),
+    path("notebook/<int:pk>/delete/", DeleteNotebookEndpoint.as_view(), name="delete_notebook")
 ]
