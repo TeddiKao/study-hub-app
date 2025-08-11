8fa69f3a089860ed4daea3900a52426f8ce74d35
Teddi <teddikao@gmail.com>
2025-08-12 07:36:13 +0800

    Add placeholder functions for notebook CRUD operations

---
+++ frontend/src/features/notes/utils/notebooks.services.ts
@@ -0,0 +1,17 @@
+async function fetchNotebooks() {
+
+}
+
+async function createNotebook() {
+
+}
+
+async function editNotebook() {
+
+}
+
+async function deleteNotebook() {
+
+}
+
+export { fetchNotebooks, createNotebook, editNotebook, deleteNotebook }
