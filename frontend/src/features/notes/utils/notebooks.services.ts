b5ee7297fc015c1057b7cec2f0718933529910ee
Teddi <teddikao@gmail.com>
2025-08-12 07:44:10 +0800

    Add owner to notebook interface

--- frontend/src/features/notes/utils/notebooks.services.ts
+++ frontend/src/features/notes/utils/notebooks.services.ts
@@ -6,6 +6,11 @@ interface Notebook {
     name: string,
     description: string,
     notebookColor: string,
+    owner: {
+        id: number,
+        email: string,
+        username: string
+    }
 }
 
 type Notebooks = Notebook[]
