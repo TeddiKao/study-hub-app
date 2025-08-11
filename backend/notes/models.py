8f66aaf61392c5c349a4a1924a79aaca7bc2af29
Teddi <teddikao@gmail.com>
2025-08-11 14:21:29 +0800

    Remove global unique=True

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Notebook(models.Model):
-    name = models.CharField(max_length=60, null=False, blank=False, unique=True)
+    name = models.CharField(max_length=60, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
     notebook_color = models.CharField(max_length=7, null=False, blank=False, default="#b3b3b3")
