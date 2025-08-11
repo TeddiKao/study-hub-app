40e0ad86e93616cef747a9b780f21585ac6252ca
Teddi <teddikao@gmail.com>
2025-08-11 16:03:39 +0800

    Add validation to prevent duplicate notebooks per owner

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -1,7 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 from .models import Notebook
 
 class NotebookSerializer(ModelSerializer):
-    class Meta:
-        model = Notebook
-        fields = "__all__"
+	def validate(self, data):
+		notebook_name = data.get("name")
+		notebook_owner = data.get("owner")
+
+		if notebook_name and notebook_owner:
+			owned_notebooks = Notebook.objects.filter(owner=notebook_owner)
+			
+			if self.instance:
+				owned_notebooks = owned_notebooks.exclude(id=self.instance.id)
+
+			identical_name_notebooks = owned_notebooks.filter(name=notebook_name)
+
+			if identical_name_notebooks.exists():
+				raise ValidationError({
+					"name": "A notebook with this name already exists"
+				})
+
+		return data
+
+	class Meta:
+		model = Notebook
+		fields = "__all__"
