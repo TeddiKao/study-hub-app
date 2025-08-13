b19b32fd7ff0ea7c56fa927a7d10ebcfefbc484d
Teddi <teddikao@gmail.com>
2025-08-13 16:54:30 +0800

    Create notebooks store

--- frontend/src/features/notes/stores/notebooks.stores.ts
+++ frontend/src/features/notes/stores/notebooks.stores.ts
@@ -1,3 +1,6 @@
+import { create } from "zustand";
+import { createNotebook, deleteNotebook, editNotebook, fetchNotebooks } from "../utils/notebooks.services";
+
 interface Notebook {
 	id: number;
 	name: string;
@@ -20,3 +23,45 @@ interface NotebookStore {
     handleNotebookEdit: (notebookId: number, notebookData: Notebook) => void
     handleNotebookDelete: (notebookId: number) => void,
 }
+
+const useNotebookStore = create<NotebookStore>((set, get) => ({
+    notebooks: [],
+
+    getNotebooks: async () => {
+        const notebookFetchResponse = await fetchNotebooks();
+        if (!notebookFetchResponse.success) {
+            return;
+        }
+
+        set({ notebooks: notebookFetchResponse.notebooks })
+    },
+
+    handleNotebookCreate: async (notebookData: Notebook) => {
+        const notebookCreateResponse = await createNotebook(notebookData);
+        if (!notebookCreateResponse.success) {
+            return;
+        }
+
+        get().getNotebooks()
+    },
+
+    handleNotebookEdit: async (notebookId: number, notebookData: Notebook) => {
+        const notebookEditResponse = await editNotebook(notebookId, notebookData)
+        if (!notebookEditResponse.success) {
+            return;
+        }
+
+        get().getNotebooks()
+    },
+
+    handleNotebookDelete: async (notebookId) => {
+        const notebookDeleteResponse = await deleteNotebook(notebookId);
+        if (!notebookDeleteResponse.success) {
+            return;
+        }
+
+        get().getNotebooks()
+    }
+}))
+
+export { useNotebookStore }
