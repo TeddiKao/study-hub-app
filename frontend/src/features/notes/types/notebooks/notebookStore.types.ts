2e9fdcfeaebc3201ca784f1ffd9d6ea645117764
Teddi <teddikao@gmail.com>
2025-08-13 20:49:17 +0800

    Move notebook store related types and interfaces to separate file

---
+++ frontend/src/features/notes/types/notebooks/notebookStore.types.ts
@@ -0,0 +1,15 @@
+interface Notebook {
+	id: number;
+	name: string;
+	description: string;
+	notebookColor: string;
+	owner: {
+		id: number;
+		email: string;
+		username: string;
+	};
+}
+
+type Notebooks = Notebook[]
+
+export type { Notebook, Notebooks }
