443a030875dbd930fc61baa7396ef0f112101465
Teddi <teddikao@gmail.com>
2025-08-14 20:10:22 +0800

    Move notebook creation button to a separate component

--- frontend/src/features/notes/pages/NotebooksPage.tsx
+++ frontend/src/features/notes/pages/NotebooksPage.tsx
@@ -20,9 +20,20 @@ function Notebook({ notebookName }: NotebookProps) {
 			</span>
 
 			<span className="flex flex-col ml-3">
-				<span className="font-semibold mt-2 text-left">{notebookName}</span>
+				<span className="font-semibold mt-2 text-left">
+					{notebookName}
+				</span>
 				<span className="text-gray-400 text-left">0 notes</span>
 			</span>
+		</button>
+	);
+}
+
+function CreateNotebookButton() {
+	return (
+		<button className="flex flex-row gap-1 items-center pl-2.5 pr-3 py-2 bg-sky-500 rounded-md shadow-xl text-white font-semibold hover:cursor-pointer">
+			<AddIcon />
+			<span className="text-white font-semibold">Create</span>
 		</button>
 	);
 }
@@ -54,10 +65,7 @@ function NotebooksPage() {
 			<div className="flex flex-col">
 				<div className="flex flex-row mt-3 gap-4">
 					<h1 className="font-bold text-3xl">Notebooks</h1>
-					<button className="flex flex-row gap-1 items-center pl-2.5 pr-3 py-2 bg-sky-500 rounded-md shadow-xl text-white font-semibold hover:cursor-pointer">
-						<AddIcon />
-						<span className="text-white font-semibold">Create</span>
-					</button>
+					<CreateNotebookButton />
 				</div>
 
 				<div className="grid grid-cols-5 gap-4 mt-2">
