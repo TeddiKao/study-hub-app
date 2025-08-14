bd6d409a321e1691f4a60c61dcfb3f5c466830d0
Teddi <teddikao@gmail.com>
2025-08-14 15:51:39 +0800

    Add basic design for notebook page

--- frontend/src/features/notes/pages/NotebooksPage.tsx
+++ frontend/src/features/notes/pages/NotebooksPage.tsx
@@ -1,11 +1,23 @@
+import NotebookIcon from "@/shared/components/icons/NotebookIcon";
 import Navbar from "@/shared/pages/DashboardPage/components/Navbar/Navbar";
 
 function NotebooksPage() {
-    return (
-        <div className="flex flex-row">
-            <Navbar />
-        </div>
-    )
+	return (
+		<div className="flex flex-row gap-4">
+			<Navbar />
+			<div className="flex flex-col">
+				<h1 className="font-bold text-3xl mt-2">Notebooks</h1>
+
+				<div className="flex flex-row">
+					<div className="flex flex-col bg-white">
+						<NotebookIcon size={80} className="" />
+                        <p className="font-semibold">History</p>
+                        <p className="text-gray-400">12 notes</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default NotebooksPage;
