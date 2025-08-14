d772f5fb350630fda769ddc7a2cfb9811082d403
Teddi <teddikao@gmail.com>
2025-08-14 18:02:10 +0800

    Ensure that button type is declared to prevent accidental submits

--- frontend/src/features/notes/pages/NotebooksPage.tsx
+++ frontend/src/features/notes/pages/NotebooksPage.tsx
@@ -9,7 +9,7 @@ function NotebooksPage() {
 				<h1 className="font-bold text-3xl mt-3">Notebooks</h1>
 
 				<div className="grid grid-cols-5 gap-4 mt-2">
-					<button className="flex flex-col pb-2 bg-white rounded-2xl shadow-xl hover:cursor-pointer">
+					<button type="button" className="flex flex-col pb-2 bg-white rounded-2xl shadow-xl">
 						<div className="w-[180px] h-[180px] rounded-t-2xl bg-gray-100 flex flex-row items-center justify-center">
 							<NotebookIcon size={120} className="" />
 						</div>
