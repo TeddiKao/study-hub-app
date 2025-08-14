9760fe0a05004e662121fe2656f53c11442fe8b4
Teddi <teddikao@gmail.com>
2025-08-15 07:52:42 +0800

    Attach value and onChange attributes to create notebook modal

--- frontend/src/features/notes/components/CreateNotebookForm.tsx
+++ frontend/src/features/notes/components/CreateNotebookForm.tsx
@@ -1,8 +1,11 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { useCreateNotebookFormStore } from "../stores/createNotebookForm.stores";
 
 function CreateNotebookForm() {
+	const { name, description, handleNameChange, handleDescriptionChange } = useCreateNotebookFormStore();
+
 	return (
 		<form className="flex flex-col p-2">
 			<div className="flex flex-col mb-2">
@@ -10,7 +13,9 @@ function CreateNotebookForm() {
 				<Input
 					type="text"
 					id="notebook-name"
+					value={name}
 					placeholder="Notebook name"
+					onChange={handleNameChange}
 				/>
 			</div>
 
@@ -19,6 +24,8 @@ function CreateNotebookForm() {
 				<Textarea
 					id="notebook-description"
 					placeholder="Briefly describe what this notebook is about"
+					value={description}
+					onChange={handleDescriptionChange}
 				/>
 			</div>
 
