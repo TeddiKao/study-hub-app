69d1d12b57413e9d50f70571a32a8448eabaaada
Teddi <teddikao@gmail.com>
2025-08-14 20:52:50 +0800

    Add dialog title and description to notebook creation dialog

--- frontend/src/features/notes/components/CreateNotebookDialog.tsx
+++ frontend/src/features/notes/components/CreateNotebookDialog.tsx
@@ -1,9 +1,15 @@
-import { DialogContent } from "@radix-ui/react-dialog";
+import { DialogContent, DialogDescription } from "@radix-ui/react-dialog";
 import CreateNotebookForm from "./CreateNotebookForm";
+import { DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
 function CreateNotebookDialog() {
     return (
         <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Create notebook</DialogTitle>
+                <DialogDescription>Create a notebook here</DialogDescription>
+            </DialogHeader>
+
             <CreateNotebookForm />
         </DialogContent>
     )
