8f3ed936771f18fd179c35be687ae7c56cc595ac
Teddi <teddikao@gmail.com>
2025-08-14 19:09:53 +0800

    Extract notebook markup and styling to a separate component and dynamically render notebook name

--- frontend/src/features/notes/pages/NotebooksPage.tsx
+++ frontend/src/features/notes/pages/NotebooksPage.tsx
@@ -3,6 +3,26 @@ import Navbar from "@/shared/pages/DashboardPage/components/Navbar/Navbar";
 import { useQuery } from "@tanstack/react-query";
 import { useNotebooksStore } from "../stores/notebooks.stores";
 
+interface NotebookProps {
+	notebookName: string,
+}
+
+function Notebook({ notebookName }: NotebookProps) {
+	<button
+		type="button"
+		className="flex flex-col pb-2 bg-white rounded-2xl shadow-xl"
+	>
+		<div className="w-[180px] h-[180px] rounded-t-2xl bg-gray-100 flex flex-row items-center justify-center">
+			<NotebookIcon size={120} className="" />
+		</div>
+
+		<div className="flex flex-col ml-3">
+			<p className="font-semibold mt-2 text-left">{notebookName}</p>
+			<p className="text-gray-400 text-left">12 notes</p>
+		</div>
+	</button>;
+}
+
 function NotebooksPage() {
 	const { notebooks, getNotebooks } = useNotebooksStore();
 
@@ -10,18 +30,18 @@ function NotebooksPage() {
 		queryKey: ["notebooks"],
 		queryFn: () => getNotebooks(),
 		staleTime: Infinity,
-		
+
 		refetchOnMount: false,
 		refetchOnWindowFocus: true,
 		refetchOnReconnect: true,
-	})
+	});
 
 	if (isLoading) {
-		return <div>Fetching notebooks ...</div>
+		return <div>Fetching notebooks ...</div>;
 	}
 
 	if (error) {
-		return <div>An error occured while fetching notebooks</div>
+		return <div>An error occured while fetching notebooks</div>;
 	}
 
 	return (
@@ -30,18 +50,7 @@ function NotebooksPage() {
 			<div className="flex flex-col">
 				<h1 className="font-bold text-3xl mt-3">Notebooks</h1>
 
-				<div className="grid grid-cols-5 gap-4 mt-2">
-					<button type="button" className="flex flex-col pb-2 bg-white rounded-2xl shadow-xl">
-						<div className="w-[180px] h-[180px] rounded-t-2xl bg-gray-100 flex flex-row items-center justify-center">
-							<NotebookIcon size={120} className="" />
-						</div>
-
-						<div className="flex flex-col ml-3">
-							<p className="font-semibold mt-2 text-left">History</p>
-							<p className="text-gray-400 text-left">12 notes</p>
-						</div>
-					</button>
-				</div>
+				<div className="grid grid-cols-5 gap-4 mt-2"></div>
 			</div>
 		</div>
 	);
