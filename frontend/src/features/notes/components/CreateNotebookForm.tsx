29b51b4e36f565fc028c69e1395b520edd7ec2f1
Teddi <teddikao@gmail.com>
2025-08-15 12:37:58 +0800

    Wrap DialogClose in create notebook button

--- frontend/src/features/notes/components/CreateNotebookForm.tsx
+++ frontend/src/features/notes/components/CreateNotebookForm.tsx
@@ -4,9 +4,16 @@ import { Textarea } from "@/components/ui/textarea";
 import { useCreateNotebookFormStore } from "../stores/createNotebookForm.stores";
 import type { FormEvent } from "react";
 import { useNotebooksStore } from "../stores/notebooks.stores";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 function CreateNotebookForm() {
-	const { name, description, handleNameChange, handleDescriptionChange, clearDetails } = useCreateNotebookFormStore();
+	const {
+		name,
+		description,
+		handleNameChange,
+		handleDescriptionChange,
+		clearDetails,
+	} = useCreateNotebookFormStore();
 	const { handleNotebookCreate } = useNotebooksStore();
 
 	async function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
@@ -15,16 +22,18 @@ function CreateNotebookForm() {
 		await handleNotebookCreate({
 			name: name,
 			description: description,
-			notebookColor: "#FFA500"
-		})
+			notebookColor: "#FFA500",
+		});
 
-		clearDetails()
+		clearDetails();
 	}
 
 	return (
 		<form className="flex flex-col p-2" onSubmit={handleFormSubmit}>
 			<div className="flex flex-col mb-2">
-				<Label htmlFor="notebook-name" className="mb-1">Name</Label>
+				<Label htmlFor="notebook-name" className="mb-1">
+					Name
+				</Label>
 				<Input
 					type="text"
 					id="notebook-name"
@@ -35,7 +44,9 @@ function CreateNotebookForm() {
 			</div>
 
 			<div className="flex flex-col mb-2">
-				<Label htmlFor="notebook-description" className="mb-1">Description</Label>
+				<Label htmlFor="notebook-description" className="mb-1">
+					Description
+				</Label>
 				<Textarea
 					id="notebook-description"
 					placeholder="Briefly describe what this notebook is about"
@@ -44,9 +55,14 @@ function CreateNotebookForm() {
 				/>
 			</div>
 
-			<button type="submit" className="bg-sky-500 w-full rounded-md text-white py-2 hover:bg-sky-700 hover:cursor-pointer">
-				Create notebook
-			</button>
+			<DialogClose asChild>
+				<button
+					type="submit"
+					className="bg-sky-500 w-full rounded-md text-white py-2 hover:bg-sky-700 hover:cursor-pointer"
+				>
+					Create notebook
+				</button>
+			</DialogClose>
 		</form>
 	);
 }
