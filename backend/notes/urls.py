92bfeec0367f92d3c999be3694f76f7ca3079a34
Teddi <teddikao@gmail.com>
2025-08-16 16:23:56 +0800

    Add URL configuration for notebook retrieving API endpoint

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreateNotebookEndpoint, FetchNotebooksEndpoint, DeleteNotebookEndpoint, EditNotebookEndpoint
+from .views import CreateNotebookEndpoint, FetchNotebooksEndpoint, DeleteNotebookEndpoint, EditNotebookEndpoint, RetrieveNotebookEndpoint
 
 urlpatterns = [
     path("notebooks/", FetchNotebooksEndpoint.as_view(), name="fetch_notebooks"),
     path("notebook/create/", CreateNotebookEndpoint.as_view(), name="create_notebook"),
+    path("notebook/<int:pk>", RetrieveNotebookEndpoint.as_view(), name="retrieve_notebook"),
     path("notebook/<int:pk>/edit/", EditNotebookEndpoint.as_view(), name="edit_notebook"),
     path("notebook/<int:pk>/delete/", DeleteNotebookEndpoint.as_view(), name="delete_notebook")
 ]
