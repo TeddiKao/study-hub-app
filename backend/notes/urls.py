350951183d86b644a61fe7ac61c83b999a561a7b
Teddi <teddikao@gmail.com>
2025-08-16 16:39:34 +0800

    Add missing trailing slash

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -4,7 +4,7 @@ from .views import CreateNotebookEndpoint, FetchNotebooksEndpoint, DeleteNoteboo
 urlpatterns = [
     path("notebooks/", FetchNotebooksEndpoint.as_view(), name="fetch_notebooks"),
     path("notebook/create/", CreateNotebookEndpoint.as_view(), name="create_notebook"),
-    path("notebook/<int:pk>", RetrieveNotebookEndpoint.as_view(), name="retrieve_notebook"),
+    path("notebook/<int:pk>/", RetrieveNotebookEndpoint.as_view(), name="retrieve_notebook"),
     path("notebook/<int:pk>/edit/", EditNotebookEndpoint.as_view(), name="edit_notebook"),
     path("notebook/<int:pk>/delete/", DeleteNotebookEndpoint.as_view(), name="delete_notebook")
 ]
