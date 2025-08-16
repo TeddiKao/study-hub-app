d814ee794964e8df6fd93a30f144b9016345cb36
Teddi <teddikao@gmail.com>
2025-08-16 14:49:21 +0800

    Add markup for edit notebook dialog

--- frontend/src/features/notes/components/editNotebook/EditNotebookDialog.tsx
+++ frontend/src/features/notes/components/editNotebook/EditNotebookDialog.tsx
@@ -1,5 +1,17 @@
-function EditNotebookDialog() {
+import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import EditNotebookForm from "./EditNotebookForm";
 
+function EditNotebookDialog() {
+	return (
+		<DialogContent>
+			<DialogHeader>
+				<DialogTitle>Edit notebook</DialogTitle>
+				<DialogDescription>Make changes to an existing notebook</DialogDescription>
+			</DialogHeader>
+
+			<EditNotebookForm />
+		</DialogContent>
+	);
 }
 
 export default EditNotebookDialog;
