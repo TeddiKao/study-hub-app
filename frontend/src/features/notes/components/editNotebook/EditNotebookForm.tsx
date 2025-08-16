c7b1d6f087a0c64830e62e6b651b35f935e01bee
Teddi <teddikao@gmail.com>
2025-08-16 14:45:49 +0800

    Copy form markup into edit notebook form

--- frontend/src/features/notes/components/editNotebook/EditNotebookForm.tsx
+++ frontend/src/features/notes/components/editNotebook/EditNotebookForm.tsx
@@ -1,5 +1,43 @@
-function EditNotebookForm() {
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
+function EditNotebookForm() {
+	return (
+		<form className="flex flex-col p-2" onSubmit={handleFormSubmit}>
+			<div className="flex flex-col mb-2">
+				<Label htmlFor="notebook-name" className="mb-1">
+					Name
+				</Label>
+				<Input
+					type="text"
+					id="notebook-name"
+					value={name}
+					placeholder="Notebook name"
+					onChange={handleNameChange}
+				/>
+			</div>
+
+			<div className="flex flex-col mb-2">
+				<Label htmlFor="notebook-description" className="mb-1">
+					Description
+				</Label>
+				<Textarea
+					id="notebook-description"
+					placeholder="Briefly describe what this notebook is about"
+					value={description}
+					onChange={handleDescriptionChange}
+				/>
+			</div>
+
+			<button
+				type="submit"
+				className="bg-sky-500 w-full rounded-md text-white py-2 hover:bg-sky-700 hover:cursor-pointer"
+			>
+				Create notebook
+			</button>
+		</form>
+	);
 }
 
 export default EditNotebookForm;
