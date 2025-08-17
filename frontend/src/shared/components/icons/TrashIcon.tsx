ec07990ec0218a526b38151f28cc71881ffcf243
Teddi <teddikao@gmail.com>
2025-08-17 13:17:28 +0800

    Create props interface for trash icon

--- frontend/src/shared/components/icons/TrashIcon.tsx
+++ frontend/src/shared/components/icons/TrashIcon.tsx
@@ -1,4 +1,9 @@
-function TrashIcon() {
+import type { Trash2 } from "lucide-react";
+import type { ComponentProps } from "react";
+
+type TrashIconProps = ComponentProps<typeof Trash2>
+
+function TrashIcon({ size = 16, className = "", ...rest }: TrashIconProps) {
 
 }
 
