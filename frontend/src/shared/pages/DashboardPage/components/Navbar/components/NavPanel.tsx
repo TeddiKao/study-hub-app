614437737cfd161237477e06f230597ec1908cb9
Teddi <teddikao@gmail.com>
2025-08-17 12:42:18 +0800

    Add logic to display notebooks on dashboard when panel is expanded

--- frontend/src/shared/pages/DashboardPage/components/Navbar/components/NavPanel.tsx
+++ frontend/src/shared/pages/DashboardPage/components/Navbar/components/NavPanel.tsx
@@ -1,29 +1,37 @@
+import { useNotebooksStore } from "@/features/notes/stores/notebooks.stores";
 import AddIcon from "@/shared/components/icons/AddIcon";
 import NotebookIcon from "@/shared/components/icons/NotebookIcon";
 import { useDashboardNavbarState } from "@/shared/stores/dashboard.stores";
 
 interface ItemProps {
-    itemType: string,
-    itemName: string,
-    color: string
+	itemType: string;
+	itemName: string;
+	color: string;
 }
 
 function Item({ itemType, itemName, color }: ItemProps) {
-    function getItemIcon() {
-        switch (itemType) {
-            case "notebook":
-                return <NotebookIcon className="fill-white" size={20} />
+	function getItemIcon() {
+		switch (itemType) {
+			case "notebook":
+				return <NotebookIcon className="fill-white" size={20} />;
 
-            default:
-                return null;
-        }
-    }
+			default:
+				return null;
+		}
+	}
 
 	return (
-		<button type="button" aria-label={itemName} className="flex flex-row mb-0.5 p-1 items-center hover:cursor-pointer hover:bg-gray-300 rounded-md">
-			<div className="p-1 rounded-sm" style={{
-                backgroundColor: color
-            }}>
+		<button
+			type="button"
+			aria-label={itemName}
+			className="flex flex-row mb-0.5 p-1 items-center hover:cursor-pointer hover:bg-gray-300 rounded-md"
+		>
+			<div
+				className="p-1 rounded-sm"
+				style={{
+					backgroundColor: color,
+				}}
+			>
 				{getItemIcon()}
 			</div>
 			<p className="ml-2">{itemName}</p>
@@ -37,11 +45,12 @@ function AddNotebookButton() {
 			<AddIcon size={16} className="fill-sky-500" />
 			<p className="text-sky-500 ml-1">Add notebook</p>
 		</button>
-	)
+	);
 }
 
 function NavPanel() {
 	const { expanded, expandedItem } = useDashboardNavbarState();
+	const { notebooks } = useNotebooksStore();
 
 	if (!(expanded && expandedItem === "notebooks")) return null;
 
@@ -53,10 +62,17 @@ function NavPanel() {
 			className="flex flex-col bg-gray-100 py-3 pl-3 pr-10"
 		>
 			<p className="text-sm text-gray-500 mb-1 pl-1">Notebooks</p>
-			
-            <div className="flex flex-row">
-                <Item itemType="notebook" itemName="History" color="hsl(0, 0%, 0%)" />
-            </div>
+
+			<div className="flex flex-row">
+				{notebooks.map(({ id, name, notebookColor }) => (
+					<Item
+						key={id}
+						itemName={name}
+						color={notebookColor}
+						itemType={expandedItem}
+					/>
+				))}
+			</div>
 
 			<AddNotebookButton />
 		</div>
