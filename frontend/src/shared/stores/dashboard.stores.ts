00a757bff8d9ba0611d377fbf43878c8006fa37d
Teddi <teddikao@gmail.com>
2025-08-18 07:07:36 +0800

    Ensure that all attribbutes of active item are cleared

--- frontend/src/shared/stores/dashboard.stores.ts
+++ frontend/src/shared/stores/dashboard.stores.ts
@@ -1,81 +1,85 @@
 import { create } from "zustand";
 
 interface DashboardNavbarStateStore {
-    expanded: boolean;
-    expandedItem: string | null
+	expanded: boolean;
+	expandedItem: string | null;
 
-    expandNavbar: (expandedItem: string) => void;
-    collapseNavbar: () => void;
+	expandNavbar: (expandedItem: string) => void;
+	collapseNavbar: () => void;
 }
 
 interface ActiveItemStore {
-    activeItemId: number | null,
-    activeItemName: string | null,
-    activeItemType: string | null,
+	activeItemId: number | null;
+	activeItemName: string | null;
+	activeItemType: string | null;
 
-    canUpdateActiveItemId: boolean,
+	canUpdateActiveItemId: boolean;
 
-    updateActiveItem: (itemId: number) => void,
-    updateActiveItemName: (itemName: string) => void,
-    updateActiveItemType: (itemType: string) => void,
+	updateActiveItem: (itemId: number) => void;
+	updateActiveItemName: (itemName: string) => void;
+	updateActiveItemType: (itemType: string) => void;
 
-    clearActiveItem: () => void,
+	clearActiveItem: () => void;
 
-    enableActiveItemIdUpdate: () => void,
-    disableActiveItemIdUpdate: () => void,
+	enableActiveItemIdUpdate: () => void;
+	disableActiveItemIdUpdate: () => void;
 }
 
 const useDashboardNavbarState = create<DashboardNavbarStateStore>((set) => ({
-    expanded: false,
-    expandedItem: null,
+	expanded: false,
+	expandedItem: null,
 
-    expandNavbar: (expandedItem: string) => {
-        set({ expanded: true, expandedItem})
-    },
+	expandNavbar: (expandedItem: string) => {
+		set({ expanded: true, expandedItem });
+	},
 
-    collapseNavbar: () => {
-        set({ expanded: false, expandedItem: null })
-    }
-}))
+	collapseNavbar: () => {
+		set({ expanded: false, expandedItem: null });
+	},
+}));
 
 const useActiveItemStore = create<ActiveItemStore>((set, get) => ({
-    activeItemId: null,
-    activeItemName: null,
-    activeItemType: null,
+	activeItemId: null,
+	activeItemName: null,
+	activeItemType: null,
 
-    canUpdateActiveItemId: true,
+	canUpdateActiveItemId: true,
 
-    updateActiveItem: (itemId: number) => {
-        if (get().canUpdateActiveItemId) {
-            set({ activeItemId: itemId })
-        }
-    },
+	updateActiveItem: (itemId: number) => {
+		if (get().canUpdateActiveItemId) {
+			set({ activeItemId: itemId });
+		}
+	},
 
-    updateActiveItemName: (itemName: string) => {
-        if (get().canUpdateActiveItemId) {
-            set({ activeItemName: itemName })
-        }
-    },
+	updateActiveItemName: (itemName: string) => {
+		if (get().canUpdateActiveItemId) {
+			set({ activeItemName: itemName });
+		}
+	},
 
-    updateActiveItemType: (itemType: string) => {
-        if (get().canUpdateActiveItemId) {
-            set({ activeItemType: itemType })
-        }
-    },
+	updateActiveItemType: (itemType: string) => {
+		if (get().canUpdateActiveItemId) {
+			set({ activeItemType: itemType });
+		}
+	},
 
-    clearActiveItem: () => {
-        if (get().canUpdateActiveItemId) {
-            set({ activeItemId: null })
-        }
-    },
+	clearActiveItem: () => {
+		if (get().canUpdateActiveItemId) {
+			set({
+				activeItemId: null,
+				activeItemName: null,
+				activeItemType: null,
+			});
+		}
+	},
 
-    enableActiveItemIdUpdate: () => {
-        set({ canUpdateActiveItemId: true })
-    },
+	enableActiveItemIdUpdate: () => {
+		set({ canUpdateActiveItemId: true });
+	},
 
-    disableActiveItemIdUpdate: () => {
-        set({ canUpdateActiveItemId: false })
-    },
-}))
+	disableActiveItemIdUpdate: () => {
+		set({ canUpdateActiveItemId: false });
+	},
+}));
 
-export { useDashboardNavbarState, useActiveItemStore }
+export { useDashboardNavbarState, useActiveItemStore };
