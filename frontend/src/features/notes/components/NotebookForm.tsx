a6692d50c122a5b62b6f217aa0a26ae572e665fa
Teddi <teddikao@gmail.com>
2025-08-17 10:00:48 +0800

    Add logic for deciding store to use

--- frontend/src/features/notes/components/NotebookForm.tsx
+++ frontend/src/features/notes/components/NotebookForm.tsx
@@ -1,10 +1,20 @@
+import { useCreateNotebookFormStore } from "../stores/createNotebookForm.stores";
+import { useEditNotebookFormStore } from "../stores/editNotebookForm.stores";
+
 interface NotebookFormProps {
     mode: "create" | "edit",
     notebookId: number
 }
 
 function NotebookForm({ mode, notebookId }: NotebookFormProps) {
+    const createNotebookFormStore = useCreateNotebookFormStore();
+    const editNotebookFormStore = useEditNotebookFormStore();
 
+    const usedStore = mode === "create" ? createNotebookFormStore : editNotebookFormStore
+    const name = usedStore.name
+    const description = usedStore.description
+    const handleNameChange = usedStore.handleNameChange
+    const handleDescriptionChange = usedStore.handleDescriptionChange
 }
 
 export default NotebookForm;
