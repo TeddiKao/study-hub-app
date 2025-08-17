541946788d0313342e5e77a33e340b01564d9992
Teddi <teddikao@gmail.com>
2025-08-17 10:19:13 +0800

    Add useQuery which only fetches for edit mode

--- frontend/src/features/notes/components/NotebookForm.tsx
+++ frontend/src/features/notes/components/NotebookForm.tsx
@@ -1,25 +1,49 @@
+import { useEffect } from "react";
 import { useCreateNotebookFormStore } from "../stores/createNotebookForm.stores";
 import { useEditNotebookFormStore } from "../stores/editNotebookForm.stores";
 import { useNotebooksStore } from "../stores/notebooks.stores";
+import { retrieveNotebook } from "../utils/notebooks.services";
+import { useQuery } from "@tanstack/react-query";
 
 interface NotebookFormProps {
-    mode: "create" | "edit",
-    notebookId: number
+	mode: "create" | "edit";
+	notebookId?: number;
 }
 
 function NotebookForm({ mode, notebookId }: NotebookFormProps) {
-    const createNotebookFormStore = useCreateNotebookFormStore();
-    const editNotebookFormStore = useEditNotebookFormStore();
+	const createNotebookFormStore = useCreateNotebookFormStore();
+	const editNotebookFormStore = useEditNotebookFormStore();
 
-    const usedStore = mode === "create" ? createNotebookFormStore : editNotebookFormStore
-    const name = usedStore.name
-    const description = usedStore.description
-    const handleNameChange = usedStore.handleNameChange
-    const handleDescriptionChange = usedStore.handleDescriptionChange
-    const updateName = usedStore.updateName
-    const updateDescription = usedStore.updateDescription
+	const usedStore =
+		mode === "create" ? createNotebookFormStore : editNotebookFormStore;
+	const name = usedStore.name;
+	const description = usedStore.description;
+	const handleNameChange = usedStore.handleNameChange;
+	const handleDescriptionChange = usedStore.handleDescriptionChange;
+	const updateName = usedStore.updateName;
+	const updateDescription = usedStore.updateDescription;
 
-    const { handleNotebookCreate, handleNotebookEdit } = useNotebooksStore();
+	const { handleNotebookCreate, handleNotebookEdit } = useNotebooksStore();
+
+	const { data, isLoading, error } = useQuery({
+		queryKey: ["notebookInfo", notebookId],
+		queryFn: async () => {
+            if (!notebookId) return;
+
+			const notebookRetrieveResponse = await retrieveNotebook(notebookId);
+			if (!notebookRetrieveResponse.success) {
+				throw new Error(notebookRetrieveResponse.error);
+			}
+
+			return notebookRetrieveResponse;
+		},
+
+		staleTime: 1000 * 5 * 60,
+
+		refetchOnReconnect: true,
+		refetchOnMount: true,
+		refetchOnWindowFocus: false,
+	});
 }
 
 export default NotebookForm;
