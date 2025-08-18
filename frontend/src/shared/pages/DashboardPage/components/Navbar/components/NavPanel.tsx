9c974412e4279c0f754d6992bcb6d53a9f335d7e
Teddi <teddikao@gmail.com>
2025-08-18 17:52:05 +0800

    Convert addIcon into a button

--- frontend/src/shared/pages/DashboardPage/components/Navbar/components/NavPanel.tsx
+++ frontend/src/shared/pages/DashboardPage/components/Navbar/components/NavPanel.tsx
@@ -203,9 +203,9 @@ function NavPanel() {
 
 					<Tooltip>
 						<TooltipTrigger asChild>
-							<div className="p-1 hover:bg-gray-300 hover:cursor-pointer rounded-md">
+							<button className="p-1 hover:bg-gray-300 hover:cursor-pointer rounded-md">
 								<AddIcon size={20} className="fill-gray-500" />
-							</div>
+							</button>
 						</TooltipTrigger>
 
 						<TooltipContent side="bottom">
