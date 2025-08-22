e47b3486fed2a02f61621f630c4620ef801b4585
Teddi <teddikao@gmail.com>
2025-08-22 17:01:17 +0800

    Integrate changes accordingly

--- frontend/src/features/notes/pages/NotebookPage/NotebooksPage.tsx
+++ frontend/src/features/notes/pages/NotebookPage/NotebooksPage.tsx
@@ -28,6 +28,7 @@ import { useEditNotebookFormStore } from "../../stores/editNotebookForm.stores";
 import NotebookDialog from "../../components/NotebookDialog";
 import NotebookDropdownMenu from "./components/NotebookDropdownmenu";
 import NotebookCard from "./components/NotebookCard";
+import CreateNotebookButton from "./components/CreateNotebookButton";
 
 interface NotebookProps {
 	notebookName: string;
@@ -37,17 +38,6 @@ interface NotebookProps {
 
 interface NotebookDropdownMenuProps {
 	notebookId: number;
-}
-
-function CreateNotebookButton() {
-	return (
-		<DialogTrigger asChild>
-			<button className="flex flex-row gap-1 items-center pl-2.5 pr-3 py-2 bg-sky-500 rounded-md shadow-xl text-white font-semibold hover:cursor-pointer">
-				<AddIcon />
-				<p className="text-white font-semibold">Create</p>
-			</button>
-		</DialogTrigger>
-	);
 }
 
 function NotebooksPage() {
