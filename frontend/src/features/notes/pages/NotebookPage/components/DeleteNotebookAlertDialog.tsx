9089d9b9f25ed02848f8e47693c3e85015ece61b
Teddi <teddikao@gmail.com>
2025-08-22 16:55:10 +0800

    Move DeleteNotebookAlertDialog component to a separate file

---
+++ frontend/src/features/notes/pages/NotebookPage/components/DeleteNotebookAlertDialog.tsx
@@ -0,0 +1,34 @@
+function DeleteNotebookAlertDialog({ notebookId }: DeleteNotebookAlertDialog) {
+	const { handleNotebookDelete } = useNotebooksStore();
+
+	return (
+		<AlertDialogContent>
+			<AlertDialogHeader>
+				<AlertDialogTitle>Delete notebook?</AlertDialogTitle>
+				<AlertDialogDescription>
+					This will permanently delete your notebook. This cannot be
+					undone
+				</AlertDialogDescription>
+			</AlertDialogHeader>
+
+			<AlertDialogFooter>
+				<AlertDialogCancel className="hover:cursor-pointer">
+					Cancel
+				</AlertDialogCancel>
+				<AlertDialogAction
+					onClick={async () => {
+						try {
+							await handleNotebookDelete(notebookId);
+						} catch (error) {
+							console.error(error);
+						}
+					}}
+					className="bg-red-500 hover:bg-red-900 hover:cursor-pointer"
+				>
+					Delete
+				</AlertDialogAction>
+			</AlertDialogFooter>
+		</AlertDialogContent>
+	);
+}
+
