5d4be53f167bacf8ee1083345cd2f5dece8de0ab
Teddi <teddikao@gmail.com>
2025-08-22 21:17:40 +0800

    Create tests for notebook-related operations

--- backend/notes/tests.py
+++ backend/notes/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Notebook
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 # Create your tests here.
+class NotebooksTestCase(TestCase):
+    def setUp(self):
+        user = get_user_model()
+        
+        self.user1 = user.objects.create(email="user1@gmail.com", username="user1", password="test")
+        self.user2 = user.objects.create(email="user2@gmail.com", username="user2", password="test")
+
+    def test_duplicate_notebook_per_owner(self):
+        def create_notebooks():
+            notebook1 = Notebook.objects.create(name="Notebook 1", description="Random description", owner=self.user1)
+            notebook2 = Notebook.objects.create(name="Notebook 1", description="Another description", owner=self.user1)
+
+        self.assertRaises(IntegrityError, create_notebooks)
+
+    def test_duplicate_notebooks_for_different_owners(self):
+        notebook1 = Notebook.objects.create(name="Notebook 1", description="Random description", owner=self.user1)
+        notebook2 = Notebook.objects.create(name="Notebook 1", description="Another description", owner=self.user2)
+            
