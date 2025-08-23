051df0dd4cb82e1977fb5c7d85dfb98b3b43a5d7
Teddi <teddikao@gmail.com>
2025-08-23 18:58:44 +0800

    Use string reference instead of actual model import to prevent circular import

--- backend/note_taking/models/note.py
+++ backend/note_taking/models/note.py
@@ -1,7 +1,6 @@
 from django.db import models
-from note_taking.models import Notebook
 
 class Note(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
-    notebook = models.ForeignKey(Notebook, on_delete=models.CASCADE)
+    notebook = models.ForeignKey("note_taking.models.Notebook", on_delete=models.CASCADE)
