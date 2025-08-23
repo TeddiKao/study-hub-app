6e046fdd2a28c579ec172198639908b000932d80
Teddi <teddikao@gmail.com>
2025-08-23 10:07:38 +0800

    Increase flexibility of dashboard layout component by allowing custom styles

--- frontend/src/shared/components/wrappers/DashboardLayout.tsx
+++ frontend/src/shared/components/wrappers/DashboardLayout.tsx
@@ -1,19 +1,24 @@
+import { cn } from "@/lib/utils";
 import Navbar from "@/shared/pages/DashboardPage/components/Navbar/Navbar";
 import type { ReactNode } from "react";
 
-interface DashboardLayoutProps {
-	children: ReactNode;
+interface ContentProps {
+    children: ReactNode;
 }
 
-interface ContentProps extends DashboardLayoutProps {}
+interface DashboardLayoutProps extends ContentProps {
+    className?: string;
+}
 
 function Content({ children }: ContentProps) {
 	return <main className="flex-1 overflow-y-auto">{children}</main>;
 }
 
-function DashboardLayout({ children }: DashboardLayoutProps) {
-	return (
-		<div className="flex flex-row h-screen overflow-hidden">
+function DashboardLayout({ children, className = "" }: DashboardLayoutProps) {
+	const baseClasses = "flex flex-row h-screen overflow-hidden"
+    
+    return (
+		<div className={cn(baseClasses, className)}>
 			<Navbar />
 			<Content>{children}</Content>
 		</div>
