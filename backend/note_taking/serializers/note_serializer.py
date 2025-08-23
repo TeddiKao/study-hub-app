e8aee5b35b0a47732f65ccfa1a702099ce997be1
Teddi <teddikao@gmail.com>
2025-08-23 19:17:20 +0800

    Use nested serializer in NotebookSerializer

--- backend/note_taking/serializers/note_serializer.py
+++ backend/note_taking/serializers/note_serializer.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 from ..models import Note
+from ..serializers import NotebookSerializer
 
 class NoteSerializer(ModelSerializer):
+    notebook = NotebookSerializer()
+
     class Meta:
         model = Note
         fields = ["id", "name", "description", "notebook"]
