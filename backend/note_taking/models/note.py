29bc209b81c64b4c10ac9acd91680dcbc1af24f8
Teddi <teddikao@gmail.com>
2025-08-23 19:04:19 +0800

    Update model reference to use correct format

--- backend/note_taking/models/note.py
+++ backend/note_taking/models/note.py
@@ -3,4 +3,4 @@ from django.db import models
 class Note(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
-    notebook = models.ForeignKey("note_taking.models.Notebook", on_delete=models.CASCADE)
+    notebook = models.ForeignKey("note_taking.Notebook", on_delete=models.CASCADE)
