f91c04ac114de8b16be899aad78edfb1d285c596
Teddi <teddikao@gmail.com>
2025-08-23 15:17:17 +0800

    Update app name in NotesConfig

--- backend/note_taking/apps.py
+++ backend/note_taking/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NotesConfig(AppConfig):
+class NoteTakingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notes'
+    name = 'note_taking'
