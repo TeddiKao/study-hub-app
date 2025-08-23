9817ce6ea13fd0bc1c634737ea573814fca7cf9a
Teddi <teddikao@gmail.com>
2025-08-23 12:15:11 +0800

    Add min-w-0 to allow shrinking of content

--- frontend/src/shared/components/wrappers/DashboardLayout.tsx
+++ frontend/src/shared/components/wrappers/DashboardLayout.tsx
@@ -11,7 +11,7 @@ interface DashboardLayoutProps extends ContentProps {
 }
 
 function Content({ children }: ContentProps) {
-	return <main className="flex-1 overflow-y-auto">{children}</main>;
+	return <main className="flex-1 overflow-y-auto min-w-0">{children}</main>;
 }
 
 function DashboardLayout({ children, className = "" }: DashboardLayoutProps) {
