94a727d4a04811208ec13a44e9e06f5d1c5ee9db
Teddi <teddikao@gmail.com>
2025-08-23 16:51:07 +0800

    Remove trailing slash

--- frontend/src/app/api.constants.ts
+++ frontend/src/app/api.constants.ts
@@ -1,4 +1,4 @@
 const NOTE_TAKING_BASE = "note_taking";
-const NOTEBOOKS_BASE = `${NOTE_TAKING_BASE}/notebooks/`
+const NOTEBOOKS_BASE = `${NOTE_TAKING_BASE}/notebooks`
 
 export { NOTE_TAKING_BASE, NOTEBOOKS_BASE };
