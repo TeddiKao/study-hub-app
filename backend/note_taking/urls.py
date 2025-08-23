0d58f4a27bbf9a299a7e93283e897269b92d0939
Teddi <teddikao@gmail.com>
2025-08-23 16:58:34 +0800

    Update folder from "urls" to "routes" to avoid naming conflicts

--- backend/note_taking/urls.py
+++ backend/note_taking/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("notebooks/", include("note_taking.urls.notebook_urls"))
+    path("notebooks/", include("note_taking.routes.notebook_urls"))
 ]
