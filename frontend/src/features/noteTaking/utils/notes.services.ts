1ce212415f2c02ff302f0c76266b17c5994146e8
Teddi <teddikao@gmail.com>
2025-08-24 19:22:28 +0800

    Modify notebook creation service function to only accept notebookid

--- frontend/src/features/noteTaking/utils/notes.services.ts
+++ frontend/src/features/noteTaking/utils/notes.services.ts
@@ -1,124 +1,158 @@
-import api from "@/app/api"
-import { NOTES_BASE } from "@/app/api.constants"
-import type { ApiErrorResponse, ApiSuccessResponse } from "@/shared/types/api.types"
+import api from "@/app/api";
+import { NOTES_BASE } from "@/app/api.constants";
+import type {
+	ApiErrorResponse,
+	ApiSuccessResponse,
+} from "@/shared/types/api.types";
 import type { Notebook } from "../types/notebooks/notebookStore.types";
-import { isAxiosError } from "axios";
+import { isAxiosError, type AxiosResponse } from "axios";
 
-interface Note {
+interface NoteResponse {
+	name: string;
+	description: string;
+	notebook: Notebook;
+}
+
+interface RawNoteData {
     name: string;
     description: string;
-    notebook: Notebook
+}
+
+interface NotePayload {
+    name: string;
+    description: string;
+    notebookId: number;
 }
 
 interface FetchNotesSuccess extends ApiSuccessResponse {
-    notes: Note[]
+	notes: NoteResponse[];
 }
 
 interface CreateNoteSuccess extends ApiSuccessResponse {
-    createdNote: Note
+	createdNote: NoteResponse;
 }
 
 interface EditNoteSuccess extends ApiSuccessResponse {
-    editedNote: Note
+	editedNote: NoteResponse;
 }
 
-async function fetchNotes(notebookId: number): Promise<FetchNotesSuccess | ApiErrorResponse> {
-    try {
-        const response = await api.get(`${NOTES_BASE}/`, {
-            params: {
-                notebookId: notebookId
-            }
-        })
+async function fetchNotes(
+	notebookId: number
+): Promise<FetchNotesSuccess | ApiErrorResponse> {
+	try {
+		const response = await api.get(`${NOTES_BASE}/`, {
+			params: {
+				notebookId: notebookId,
+			},
+		});
 
-        return {
-            success: true,
-            message: "Successfully fetched notes",
-            notes: response.data
-        }
-    } catch (error) {
-        if (!isAxiosError(error)) {
-            return {
-                success: false,
-                error: "Failed to fetch notes"
-            }
-        }
+		return {
+			success: true,
+			message: "Successfully fetched notes",
+			notes: response.data,
+		};
+	} catch (error) {
+		if (!isAxiosError(error)) {
+			return {
+				success: false,
+				error: "Failed to fetch notes",
+			};
+		}
 
-        return {
-            success: false,
-            error: error.response?.data?.error ?? "Failed to fetch notes"
-        }
-    }
+		return {
+			success: false,
+			error: error.response?.data?.error ?? "Failed to fetch notes",
+		};
+	}
 }
 
-async function createNote(noteData: Note): Promise<CreateNoteSuccess | ApiErrorResponse> {
-    try {
-        const response = await api.post(`${NOTES_BASE}/note/create/`, noteData)
-
-        return {
-            success: true,
-            message: "Successfully created note",
-            createdNote: response.data
-        }
-    } catch (error) {
-        if (!isAxiosError(error)) {
-            return {
-                success: false,
-                error: "Failed to create note"
+async function createNote(
+	noteData: RawNoteData,
+	notebookId: number
+): Promise<CreateNoteSuccess | ApiErrorResponse> {
+	try {
+		const response = await api.post<NoteResponse, AxiosResponse<NoteResponse>, NotePayload>(
+			`${NOTES_BASE}/note/create/`,
+			{
+                name: noteData.name,
+                description: noteData.description,
+                notebookId: notebookId,
             }
-        }
+		);
 
-        return {
-            success: false,
-            error: error.response?.data?.error ?? "Failed to create note"
-        }
-    }
+		return {
+			success: true,
+			message: "Successfully created note",
+			createdNote: response.data,
+		};
+	} catch (error) {
+		if (!isAxiosError(error)) {
+			return {
+				success: false,
+				error: "Failed to create note",
+			};
+		}
+
+		return {
+			success: false,
+			error: error.response?.data?.error ?? "Failed to create note",
+		};
+	}
 }
 
-async function editNote(noteId: number, newNoteData: Note): Promise<EditNoteSuccess | ApiErrorResponse> {
-    try {
-        const response = await api.put(`${NOTES_BASE}/note/${noteId}/edit/`, newNoteData)
+async function editNote(
+	noteId: number,
+	newNoteData: NoteResponse
+): Promise<EditNoteSuccess | ApiErrorResponse> {
+	try {
+		const response = await api.put(
+			`${NOTES_BASE}/note/${noteId}/edit/`,
+			newNoteData
+		);
 
-        return {
-            success: true,
-            message: "Note edited successfully",
-            editedNote: response.data
-        }
-    } catch (error) {
-        if (!isAxiosError(error)) {
-            return {
-                success: false,
-                error: "Failed to edit note"
-            }
-        }
+		return {
+			success: true,
+			message: "Note edited successfully",
+			editedNote: response.data,
+		};
+	} catch (error) {
+		if (!isAxiosError(error)) {
+			return {
+				success: false,
+				error: "Failed to edit note",
+			};
+		}
 
-        return {
-            success: false,
-            error: error.response?.data?.error ?? "Failed to edit note"
-        }
-    }
+		return {
+			success: false,
+			error: error.response?.data?.error ?? "Failed to edit note",
+		};
+	}
 }
 
-async function deleteNote(noteId: number): Promise<ApiSuccessResponse | ApiErrorResponse> {
-    try {
-        await api.delete(`${NOTES_BASE}/note/${noteId}/delete/`)
+async function deleteNote(
+	noteId: number
+): Promise<ApiSuccessResponse | ApiErrorResponse> {
+	try {
+		await api.delete(`${NOTES_BASE}/note/${noteId}/delete/`);
 
-        return {
-            success: true,
-            message: "Note deleted successfully"
-        }
-    } catch (error) {
-        if (!isAxiosError(error)) {
-            return {
-                success: false,
-                error: "Failed to delete note"
-            }
-        }
+		return {
+			success: true,
+			message: "Note deleted successfully",
+		};
+	} catch (error) {
+		if (!isAxiosError(error)) {
+			return {
+				success: false,
+				error: "Failed to delete note",
+			};
+		}
 
-        return {
-            success: false,
-            error: error.response?.data?.error ?? "Failed to delete note"
-        }
-    }
+		return {
+			success: false,
+			error: error.response?.data?.error ?? "Failed to delete note",
+		};
+	}
 }
 
-export { fetchNotes, createNote, deleteNote, editNote }
+export { fetchNotes, createNote, deleteNote, editNote };
