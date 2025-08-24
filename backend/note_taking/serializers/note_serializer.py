d5755a78ffb75ce9282ba948d19302fcfcc03b18
Teddi <teddikao@gmail.com>
2025-08-24 09:59:22 +0800

    Create serializer-level validation for note name uniqueness

--- backend/note_taking/serializers/note_serializer.py
+++ backend/note_taking/serializers/note_serializer.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, ValidationError
 from ..models import Note, Notebook
 from ..serializers import NotebookSerializer
 
@@ -8,6 +8,29 @@ class NoteSerializer(ModelSerializer):
         queryset=Notebook.objects.all(),
         source="notebook",
     )
+
+    def validate(self, data):
+        note_name = data.get("request")
+        notebook_id = data.get("notebook_id")
+
+        if not note_name and self.instance and self.partial:
+            note_name = self.instance.name
+
+        if note_name and notebook_id:
+            notes = Note.objects.filter(notebook=notebook_id)
+
+            if self.instance:
+                notes = notes.exclude(id=self.instance.id)
+
+            identical_name_notes = notes.filter(name=note_name)
+
+            if identical_name_notes.exists():
+                raise ValidationError({
+                    "name": "A note with that name already exists!"
+                })
+            
+        return data
+
 
     class Meta:
         model = Note
