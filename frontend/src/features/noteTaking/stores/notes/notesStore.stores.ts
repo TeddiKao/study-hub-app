337aab6b4db9408e4498f612918f6d9863f0521f
Teddi <teddikao@gmail.com>
2025-08-24 19:10:33 +0800

    Create interface for notes store

---
+++ frontend/src/features/noteTaking/stores/notes/notesStore.stores.ts
@@ -0,0 +1,19 @@
+import type { Notebook } from "../../types/notebooks/notebookStore.types";
+
+interface Note {
+    name: string;
+    description: string;
+    notebook: Notebook
+}
+
+interface NotesStore {
+    notes: Note[],
+
+    updateNotes: (newNotes: Note[]) => void
+
+    getNotes: () => void
+
+    handleNoteCreate: (createdNote: Note) => void,
+    handleNoteEdit: (noteId: number, newNoteData: Note) => void,
+    handleNoteDelete: (noteId: number) => void,
+}
