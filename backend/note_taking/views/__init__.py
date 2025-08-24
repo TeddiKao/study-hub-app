d44c9a5f1a003ec6fe64d01a52b9c1012fd7cf77
Teddi <teddikao@gmail.com>
2025-08-24 12:32:29 +0800

    Re-export note editing endpoint in __init__.py

--- backend/note_taking/views/__init__.py
+++ backend/note_taking/views/__init__.py
@@ -1,4 +1,4 @@
 from .notebook_views import CreateNotebookEndpoint, EditNotebookEndpoint, DeleteNotebookEndpoint, FetchNotebooksEndpoint, RetrieveNotebookEndpoint
-from .note_views import FetchNotesEndpoint, CreateNoteEndpoint, DeleteNoteEndpoint
+from .note_views import FetchNotesEndpoint, CreateNoteEndpoint, DeleteNoteEndpoint, EditNoteEndpoint
 
-__all__ = ["CreateNotebookEndpoint", "EditNotebookEndpoint", "DeleteNotebookEndpoint", "FetchNotebooksEndpoint", "RetrieveNotebookEndpoint", "FetchNotesEndpoint", "CreateNoteEndpoint", "DeleteNoteEndpoint"]
+__all__ = ["CreateNotebookEndpoint", "EditNotebookEndpoint", "DeleteNotebookEndpoint", "FetchNotebooksEndpoint", "RetrieveNotebookEndpoint", "FetchNotesEndpoint", "CreateNoteEndpoint", "DeleteNoteEndpoint", "EditNoteEndpoint"]
