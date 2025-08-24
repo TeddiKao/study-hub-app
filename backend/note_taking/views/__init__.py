c3f93bac072aae2b3c2ddb0ca11b32592ef0f053
Teddi <teddikao@gmail.com>
2025-08-24 10:46:45 +0800

    Add endpoint for fetching notes to init.py re-export

--- backend/note_taking/views/__init__.py
+++ backend/note_taking/views/__init__.py
@@ -1,3 +1,4 @@
 from .notebook_views import CreateNotebookEndpoint, EditNotebookEndpoint, DeleteNotebookEndpoint, FetchNotebooksEndpoint, RetrieveNotebookEndpoint
+from .note_views import FetchNotesEndpoint
 
-__all__ = ["CreateNotebookEndpoint", "EditNotebookEndpoint", "DeleteNotebookEndpoint", "FetchNotebooksEndpoint", "RetrieveNotebookEndpoint"]
+__all__ = ["CreateNotebookEndpoint", "EditNotebookEndpoint", "DeleteNotebookEndpoint", "FetchNotebooksEndpoint", "RetrieveNotebookEndpoint", "FetchNotesEndpoint"]
