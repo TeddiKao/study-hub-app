9fde2f8e303cd6adc9f01f5cf5bf7bf4de7fe8f3
Teddi <teddikao@gmail.com>
2025-08-24 10:44:06 +0800

    Create endpoint for creating note

--- backend/note_taking/views/note_views.py
+++ backend/note_taking/views/note_views.py
@@ -2,10 +2,14 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from ..serializers import NoteSerializer
+from ..models import Note
 
 class FetchNotesEndpoint(ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = NoteSerializer
 
     def get_queryset(self):
-        pass
+        notebook_id = self.request.query_params.get("notebook_id")
+        queryset = Note.objects.filter(notebook=notebook_id)
+
+        return queryset
