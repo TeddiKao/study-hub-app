f7d938e48e0836319e9da320cd18a92188dc2007
Teddi <teddikao@gmail.com>
2025-08-24 19:16:26 +0800

    Create store function to get notes and update state

--- frontend/src/features/noteTaking/stores/notes/notesStore.stores.ts
+++ frontend/src/features/noteTaking/stores/notes/notesStore.stores.ts
@@ -1,18 +1,48 @@
+import { create } from "zustand";
 import type { Notebook } from "../../types/notebooks/notebookStore.types";
+import { fetchNotes } from "../../utils/notes.services";
 
 interface Note {
-    name: string;
-    description: string;
-    notebook: Notebook
+	name: string;
+	description: string;
+	notebook: Notebook;
 }
 
 interface NotesStore {
-    notes: Note[],
-    updateNotes: (newNotes: Note[]) => void
+	notes: Note[];
+	currentNotebookId: number | null;
 
-    getNotes: () => void
+	updateNotes: (newNotes: Note[]) => void;
+	updateCurrentNotebookId: (newNotebookId: number) => void;
 
-    handleNoteCreate: (createdNote: Note) => void,
-    handleNoteEdit: (noteId: number, newNoteData: Note) => void,
-    handleNoteDelete: (noteId: number) => void,
+	getNotes: () => void;
+
+	handleNoteCreate: (createdNote: Note) => void;
+	handleNoteEdit: (noteId: number, newNoteData: Note) => void;
+	handleNoteDelete: (noteId: number) => void;
 }
+
+const useNotesStore = create<NotesStore>((set, get) => ({
+	notes: [],
+	currentNotebookId: null,
+	updateNotes: (newNotes: Note[]) => set({ notes: newNotes }),
+	updateCurrentNotebookId: (newNotebookId: number) =>
+		set({ currentNotebookId: newNotebookId }),
+
+	getNotes: async () => {
+        if (!get().currentNotebookId) return;
+
+        const notebookFetchResponse = await fetchNotes(get().currentNotebookId!)
+        if (!notebookFetchResponse.success) {
+            throw new Error(notebookFetchResponse.error)
+        }
+
+        get().updateNotes(notebookFetchResponse.notes);
+    },
+
+	handleNoteCreate: () => {},
+
+	handleNoteEdit: () => {},
+
+	handleNoteDelete: () => {},
+}));
