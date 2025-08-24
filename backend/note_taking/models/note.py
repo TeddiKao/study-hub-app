6f0727fe170fa9e3a012d79fc65989037748157d
Teddi <teddikao@gmail.com>
2025-08-24 09:49:43 +0800

    Create unique constraint for note model

--- backend/note_taking/models/note.py
+++ backend/note_taking/models/note.py
@@ -4,3 +4,11 @@ class Note(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
     notebook = models.ForeignKey("note_taking.Notebook", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["notebook", "name"],
+                name="unique_note_per_notebook"
+            )
+        ]
