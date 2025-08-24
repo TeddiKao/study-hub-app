e46f3db6216219d3db7e848f32c791ff1ebe4ca2
Teddi <teddikao@gmail.com>
2025-08-24 15:14:30 +0800

    Add appropriate types

--- frontend/src/features/noteTaking/utils/notes.services.ts
+++ frontend/src/features/noteTaking/utils/notes.services.ts
@@ -1,5 +1,28 @@
-function fetchNotes(notebookId: number) {
+import api from "@/app/api"
+import { NOTES_BASE } from "@/app/api.constants"
+import type { ApiErrorResponse, ApiSuccessResponse } from "@/shared/types/api.types"
+import type { Notebook } from "../types/notebooks/notebookStore.types";
 
+interface Note {
+    name: string;
+    description: string;
+    notebook: Notebook
+}
+
+interface FetchNotesSuccess extends ApiSuccessResponse {
+    notes: Note[]
+}
+
+async function fetchNotes(notebookId: number): Promise<FetchNotesSuccess | ApiErrorResponse> {
+    try {
+        const response = await api.get(`${NOTES_BASE}/`, {
+            params: {
+                notebookId: notebookId
+            }
+        })
+
+
+    }
 }
 
 export { fetchNotes }
