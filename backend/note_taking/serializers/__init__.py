0f9b6a845dfbce52c72910328ba4b05a6524ad46
Teddi <teddikao@gmail.com>
2025-08-24 10:40:11 +0800

    Add NoteSerializer to serializer re-export

--- backend/note_taking/serializers/__init__.py
+++ backend/note_taking/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .notebook_serializer import NotebookSerializer
+from .note_serializer import NoteSerializer
 
-__all__ = ["NotebookSerializer"]
+__all__ = ["NotebookSerializer", "NoteSerializer"]
