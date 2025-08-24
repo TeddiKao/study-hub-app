c817fc42ceb2a9fbfc2609e6302661a43e6070ea
Teddi <teddikao@gmail.com>
2025-08-24 09:41:28 +0800

    Ensure that GET requests return detailed info and POST requests only require the ID

--- backend/note_taking/serializers/note_serializer.py
+++ backend/note_taking/serializers/note_serializer.py
@@ -1,10 +1,14 @@
-from rest_framework.serializers import ModelSerializer
-from ..models import Note
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from ..models import Note, Notebook
 from ..serializers import NotebookSerializer
 
 class NoteSerializer(ModelSerializer):
-    notebook = NotebookSerializer()
+    notebook = NotebookSerializer(read_only=True)
+    notebook_id = PrimaryKeyRelatedField(
+        queryset=Notebook.objects.all(),
+        source="notebook",
+    )
 
     class Meta:
         model = Note
-        fields = ["id", "name", "description", "notebook"]
+        fields = ["id", "name", "description", "notebook", "notebook_id"]
