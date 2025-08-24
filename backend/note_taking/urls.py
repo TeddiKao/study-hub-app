448c342eaae49ecf00119acee475fdf33f1d963b
Teddi <teddikao@gmail.com>
2025-08-24 10:45:00 +0800

    Create URL configuration for all note-related endpoints

--- backend/note_taking/urls.py
+++ backend/note_taking/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("notebooks/", include("note_taking.routes.notebook_urls"))
+    path("notebooks/", include("note_taking.routes.notebook_urls")),
+    path("notes/", include("note_taking.routes.note_urls"))
 ]
