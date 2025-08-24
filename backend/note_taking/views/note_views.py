86887b9189f6798b40f27213b40767c181ea683e
Teddi <teddikao@gmail.com>
2025-08-24 11:27:37 +0800

    Add validation for notebook query parameter

--- backend/note_taking/views/note_views.py
+++ backend/note_taking/views/note_views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ValidationError
 
 from ..serializers import NoteSerializer
 from ..models import Note
@@ -9,7 +10,17 @@ class FetchNotesEndpoint(ListAPIView):
     serializer_class = NoteSerializer
 
     def get_queryset(self):
-        notebook_id = self.request.query_params.get("notebook_id")
+        raw = self.request.query_params.get("notebook_id")
+        if raw is None:
+            raise ValidationError({
+                "notebook_id": "This query parameter is required"
+            })
+        
+        try:
+            notebook_id = int(raw)
+        except (ValueError, TypeError):
+            raise ValidationError({ "notebook_id": "Must be an integer" })
+
         queryset = Note.objects.filter(notebook=notebook_id)
 
         return queryset
