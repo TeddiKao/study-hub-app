cac4cde3a094cb283f512b09a04eba43308aa063
Teddi <teddikao@gmail.com>
2025-08-24 13:10:15 +0800

    Add missing trailing slash

--- backend/note_taking/routes/note_urls.py
+++ backend/note_taking/routes/note_urls.py
@@ -4,7 +4,7 @@ from ..views import FetchNotesEndpoint, CreateNoteEndpoint, DeleteNoteEndpoint, 
 urlpatterns = [
     path("", FetchNotesEndpoint.as_view(), name="fetch_notes"),
     path("note/create/", CreateNoteEndpoint.as_view(), name="create_note"),
-    path("note/<int:pk>", RetrieveNoteEndpoint.as_view(), name="retrieve_note"),
+    path("note/<int:pk>/", RetrieveNoteEndpoint.as_view(), name="retrieve_note"),
     path("note/<int:pk>/edit/", EditNoteEndpoint.as_view(), name="edit_note"),
     path("note/<int:pk>/delete/", DeleteNoteEndpoint.as_view(), name="delete_note"),
 ]
