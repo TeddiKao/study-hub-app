fd860cac4e5768ba196cf19b79d63e306890b4cb
Teddi <teddikao@gmail.com>
2025-08-25 20:56:16 +0800

    Add styling for page header button

--- frontend/src/features/noteTaking/pages/NotesPage.tsx
+++ frontend/src/features/noteTaking/pages/NotesPage.tsx
@@ -1,13 +1,21 @@
+import { Button } from "@/components/ui/button";
+import AddIcon from "@/shared/components/icons/AddIcon";
 import DashboardLayout from "@/shared/components/wrappers/DashboardLayout";
 
 function NotesPage() {
-    return (
-        <DashboardLayout className="gap-4">
-            <div className="flex flex-row items-center mt-3">
-                <h1 className="text-3xl font-semibold">Notes</h1>
-            </div>
-        </DashboardLayout>
-    )
+	return (
+		<DashboardLayout className="gap-4">
+			<div className="flex flex-row items-center justify-between mt-3 pr-8">
+				<h1 className="text-3xl font-semibold">Notes</h1>
+				<Button
+					type="button"
+					className="bg-sky-500 hover:bg-sky-700 hover:cursor-pointer"
+				>
+					<AddIcon /> <span className="-ml-0.5">New note</span>
+				</Button>
+			</div>
+		</DashboardLayout>
+	);
 }
 
 export default NotesPage;
