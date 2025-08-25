8dc7ef12161acb451e8a5d9fbbefa15aa6ad0069
Teddi <teddikao@gmail.com>
2025-08-25 20:35:33 +0800

    Add page title

--- frontend/src/features/noteTaking/pages/NotesPage.tsx
+++ frontend/src/features/noteTaking/pages/NotesPage.tsx
@@ -2,8 +2,10 @@ import DashboardLayout from "@/shared/components/wrappers/DashboardLayout";
 
 function NotesPage() {
     return (
-        <DashboardLayout>
-            <></>
+        <DashboardLayout className="gap-4">
+            <div className="flex flex-row items-center mt-3">
+                <h1 className="text-3xl font-semibold">Notes</h1>
+            </div>
         </DashboardLayout>
     )
 }
