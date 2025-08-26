c83ccab0f67092f6ff6238ddd01852e9b5a30f92
Teddi <teddikao@gmail.com>
2025-08-26 12:39:28 +0800

    Add basic markup for note card

--- frontend/src/features/noteTaking/pages/NotesPage.tsx
+++ frontend/src/features/noteTaking/pages/NotesPage.tsx
@@ -1,18 +1,27 @@
 import { Button } from "@/components/ui/button";
 import AddIcon from "@/shared/components/icons/AddIcon";
+import KebabMenuIcon from "@/shared/components/icons/KebabMenuIcon";
 import DashboardLayout from "@/shared/components/wrappers/DashboardLayout";
+import { StickyNote } from "lucide-react";
 
 function NotesPage() {
 	return (
 		<DashboardLayout className="gap-4">
-			<div className="flex flex-row items-center justify-between mt-3 pr-8">
-				<h1 className="text-3xl font-semibold">Notes</h1>
-				<Button
-					type="button"
-					className="hover:cursor-pointer"
-				>
-					<AddIcon /> <span className="-ml-0.5">New note</span>
-				</Button>
+			<div className="flex flex-col">
+				<div className="flex flex-row items-center justify-between mt-3 pr-8">
+					<h1 className="text-3xl font-semibold">Notes</h1>
+					<Button type="button" className="hover:cursor-pointer">
+						<AddIcon /> <span className="-ml-0.5">New note</span>
+					</Button>
+				</div>
+
+				<div className="flex flex-col">
+					<div className="flex flex-row bg-white shadow-md">
+						<StickyNote className="w-6 h-6" />
+						<p>Chapter 5</p>
+						<KebabMenuIcon className="w-6 h-6" />
+					</div>
+				</div>
 			</div>
 		</DashboardLayout>
 	);
