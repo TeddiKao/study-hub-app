4611e95f71eb54b9c6a68dc6768edd87fbf325e4
Teddi <teddikao@gmail.com>
2025-08-27 12:19:17 +0800

    Create custom hook to encapuslate note querying logic

--- frontend/src/features/noteTaking/hooks/query/useNotesQuery.hooks.ts
+++ frontend/src/features/noteTaking/hooks/query/useNotesQuery.hooks.ts
@@ -1,5 +1,27 @@
-function useNotesQuery() {
+import { isNullOrUndefined } from "@/shared/utils/types.utils";
+import { useQuery } from "@tanstack/react-query";
+import { fetchNotes } from "../../utils/notes.services";
+import { useNotesStore } from "../../stores/notes/notesStore.stores";
 
+function useNotesQuery() {
+    const { currentNotebookId } = useNotesStore();
+    
+    return useQuery({
+		queryKey: !isNullOrUndefined(currentNotebookId)
+			? ["notes", currentNotebookId]
+			: [],
+		queryFn: async () => {
+			const fetchNotesReponse = await fetchNotes(currentNotebookId!);
+			if (!fetchNotesReponse.success) {
+				throw new Error(fetchNotesReponse.error);
+			}
+
+			return fetchNotesReponse.notes;
+		},
+
+		enabled: !!currentNotebookId,
+		staleTime: 2 * 60 * 1000,
+	});
 }
 
 export default useNotesQuery;
