d83eac42d8bc6ba618bd6f8a0156f5213ad6cfd4
Teddi <teddikao@gmail.com>
2025-08-28 15:53:03 +0800

    Remove unused import of useEditNoteDialogStore from NoteForm component

--- frontend/src/features/noteTaking/components/NoteForm.tsx
+++ frontend/src/features/noteTaking/components/NoteForm.tsx
@@ -4,7 +4,7 @@ import { Textarea } from "@/components/ui/textarea";
 import type { FormEvent } from "react";
 import { useNotesStore } from "../stores/notes/notesStore.stores";
 import { useNoteFormStore } from "../stores/notes/noteForm.stores";
-import { useCreateNoteDialogStore, useEditNoteDialogStore } from "../stores/notes/noteDialog.stores";
+import { useCreateNoteDialogStore } from "../stores/notes/noteDialog.stores";
 
 interface NoteFormProps {
 	mode: "create" | "edit";
