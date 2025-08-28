306912ea153c4e94c260b261c212265f536192d0
Teddi <teddikao@gmail.com>
2025-08-28 14:35:10 +0800

    Remove isVisible state and updateFormVisibility method from NoteFormStore for simplification

--- frontend/src/features/noteTaking/stores/notes/noteForm.stores.ts
+++ frontend/src/features/noteTaking/stores/notes/noteForm.stores.ts
@@ -3,13 +3,10 @@ import { create } from 'zustand';
 interface NoteFormStore {
     name: string;
     description: string;
-    isVisible: boolean;
 
     updateName: (name: string) => void;
     updateDescription: (description: string) => void;
     clearDetails: () => void;
-
-    updateFormVisibility: (isVisible: boolean) => void;
 }
 
 const useNoteFormStore = create<NoteFormStore>((set) => ({
@@ -20,8 +17,6 @@ const useNoteFormStore = create<NoteFormStore>((set) => ({
     updateName: (name) => set({ name }),
     updateDescription: (description) => set({ description }),
     clearDetails: () => set({ name: "", description: "" }),
-
-    updateFormVisibility: (isVisible) => set({ isVisible: isVisible }),
 }));
 
 export { useNoteFormStore }
