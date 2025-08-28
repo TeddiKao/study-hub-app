402912a95f6824b6779a8c4899e1ccdaf54fe2d1
Teddi <teddikao@gmail.com>
2025-08-28 14:21:18 +0800

    Refactor CreateNotebookFormStore to include isVisible state and update logic

--- frontend/src/features/noteTaking/stores/notes/createNotebookForm.stores.ts
+++ frontend/src/features/noteTaking/stores/notes/createNotebookForm.stores.ts
@@ -1,9 +1,27 @@
+import { create } from 'zustand';
+
 interface CreateNotebookFormStore {
     name: string;
     description: string;
+    isVisible: boolean;
+
     updateName: (name: string) => void;
     updateDescription: (description: string) => void;
     clearDetails: () => void;
 
     updateFormVisibility: (isVisible: boolean) => void;
 }
+
+const useCreateNotebookFormStore = create<CreateNotebookFormStore>((set) => ({
+    name: "",
+    description: "",
+    isVisible: false,
+
+    updateName: (name) => set({ name }),
+    updateDescription: (description) => set({ description }),
+    clearDetails: () => set({ name: '', description: '' }),
+
+    updateFormVisibility: (isVisible) => set({ isVisible: isVisible }),
+}));
+
+export { useCreateNotebookFormStore }
