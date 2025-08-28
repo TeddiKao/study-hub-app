beba76e0e12d4def335b634341284d9eaa1a01c4
Teddi <teddikao@gmail.com>
2025-08-28 14:59:53 +0800

    Refactor NoteForm to use separate dialog store for creating notes

--- frontend/src/features/noteTaking/components/NoteForm.tsx
+++ frontend/src/features/noteTaking/components/NoteForm.tsx
@@ -4,7 +4,7 @@ import { Textarea } from "@/components/ui/textarea";
 import type { FormEvent } from "react";
 import { useNotesStore } from "../stores/notes/notesStore.stores";
 import { useNoteFormStore } from "../stores/notes/noteForm.stores";
-import { useNoteDialogStore } from "../stores/notes/noteDialog.stores";
+import { useCreateNoteDialogStore, useEditNoteDialogStore } from "../stores/notes/noteDialog.stores";
 
 interface NoteFormProps {
 	mode: "create" | "edit";
@@ -14,7 +14,8 @@ interface NoteFormProps {
 function NoteForm({ mode, noteId }: NoteFormProps) {
 	const { handleNoteCreate } = useNotesStore();
 	const { name, description, updateName, updateDescription, clearDetails } = useNoteFormStore();
-	const { closeDialog } = useNoteDialogStore();
+
+	const { closeDialog: closeCreateNoteDialog } = useCreateNoteDialogStore();
 
 	const nameFieldPlaceholder = mode === "create" ? "Note name" : "New name";
 	const descriptionFieldPlaceholder =
@@ -30,7 +31,8 @@ function NoteForm({ mode, noteId }: NoteFormProps) {
 			});
 
 			clearDetails();
-			closeDialog();
+			
+			closeCreateNoteDialog();
 		} catch (error) {
 			console.error("Error creating note:", error);
 			// Optionally, set an error state here to display an error message to the user
