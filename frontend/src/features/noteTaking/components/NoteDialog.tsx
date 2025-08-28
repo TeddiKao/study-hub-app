070bd4063de1c4266372e27ee59f6d56cfd9934b
Teddi <teddikao@gmail.com>
2025-08-28 15:02:29 +0800

    Refactor NoteDialog to use separate dialog stores for create and edit modes

--- frontend/src/features/noteTaking/components/NoteDialog.tsx
+++ frontend/src/features/noteTaking/components/NoteDialog.tsx
@@ -2,50 +2,67 @@ import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { isNullOrUndefined } from "@/shared/utils/types.utils";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import NoteForm from "./NoteForm";
-import { useNoteDialogStore } from "../stores/notes/noteDialog.stores";
+import { useEditNoteDialogStore, useCreateNoteDialogStore } from "../stores/notes/noteDialog.stores";
 
 interface NoteDialogProps {
-    mode: "create" | "edit";
-    noteId?: number;
+	mode: "create" | "edit";
+	noteId?: number;
 }
 
 type NoteDialogContentProps = NoteDialogProps;
 
 function NoteDialogContent({ mode, noteId }: NoteDialogContentProps) {
-    const dialogTitle = mode === "create" ? "Create note" : "Edit note";
-    const dialogDescription =
-        mode === "create"
-            ? "Create a note here"
-            : "Make changes to an existing note";
+	const dialogTitle = mode === "create" ? "Create note" : "Edit note";
+	const dialogDescription =
+		mode === "create"
+			? "Create a note here"
+			: "Make changes to an existing note";
 
-    if (mode === "edit" && isNullOrUndefined(noteId)) {
-        throw new Error("NoteDialog: noteId is required in edit mode");
-    }
+	if (mode === "edit" && isNullOrUndefined(noteId)) {
+		throw new Error("NoteDialog: noteId is required in edit mode");
+	}
 
-    return (
-        <DialogContent>
-            <DialogTitle>{dialogTitle}</DialogTitle>
-            <DialogDescription>{dialogDescription}</DialogDescription>
-            
-            <NoteForm mode={mode} noteId={noteId} />
-        </DialogContent>
-    );
+	return (
+		<DialogContent>
+			<DialogTitle>{dialogTitle}</DialogTitle>
+			<DialogDescription>{dialogDescription}</DialogDescription>
+
+			<NoteForm mode={mode} noteId={noteId} />
+		</DialogContent>
+	);
 }
 
 function NoteDialog({ mode, noteId }: NoteDialogProps) {
-    const { visible, showDialog, closeDialog } = useNoteDialogStore();
+	const editNotebookDialogStore = useEditNoteDialogStore();
+	const createNoteDialogStore = useCreateNoteDialogStore();
 
-    return (
-        <Dialog open={visible} onOpenChange={(open: boolean) => {
-            if (open) {
-                showDialog();
-            } else {
-                closeDialog();
-            }
-        }}>
-            <NoteDialogContent mode={mode} noteId={noteId} />
-        </Dialog>
-    );
+	const visible =
+		mode === "create"
+			? createNoteDialogStore.visible
+			: editNotebookDialogStore.visible;
+	const closeDialog =
+		mode === "create"
+			? createNoteDialogStore.closeDialog
+			: editNotebookDialogStore.closeDialog;
+	const showDialog =
+		mode === "create"
+			? createNoteDialogStore.showDialog
+			: editNotebookDialogStore.showDialog;
+
+	return (
+		<Dialog
+			open={visible}
+			onOpenChange={(open: boolean) => {
+				if (open) {
+					showDialog();
+				} else {
+					closeDialog();
+				}
+			}}
+		>
+			<NoteDialogContent mode={mode} noteId={noteId} />
+		</Dialog>
+	);
 }
 
 export default NoteDialog;
