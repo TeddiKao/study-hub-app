42af51d440186641e1d7a9cc51a1d92b41756c02
Teddi <teddikao@gmail.com>
2025-08-28 12:30:37 +0800

    Implement NoteDialog component with create and edit modes

--- frontend/src/features/noteTaking/components/NoteDialog.tsx
+++ frontend/src/features/noteTaking/components/NoteDialog.tsx
@@ -1,5 +1,30 @@
-function NoteDialog() {
-  return <></>
+import { DialogContent, DialogTitle } from "@/components/ui/dialog";
+import { isNullOrUndefined } from "@/shared/utils/types.utils";
+import { DialogDescription } from "@radix-ui/react-dialog";
+
+interface NoteDialogProps {
+    mode: "create" | "edit";
+    noteId?: number;
+}
+
+function NoteDialog({ mode, noteId }: NoteDialogProps) {
+    const dialogTitle = mode === "create" ? "Create note" : "Edit note";
+    const dialogDescription =
+        mode === "create"
+            ? "Create a note here"
+            : "Make changes to an existing note";
+
+    if (mode === "edit" && isNullOrUndefined(noteId)) {
+        throw new Error("NoteDialog: noteId is required in edit mode");
+    }
+
+    return (
+        <DialogContent>
+            <DialogTitle>{dialogTitle}</DialogTitle>
+            <DialogDescription>{dialogDescription}</DialogDescription>
+            {/* NoteForm component would go here */}
+        </DialogContent>
+    );
 }
 
 export default NoteDialog;
