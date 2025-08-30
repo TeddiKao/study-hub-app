34623923c8f503e663822dc0051b9a31c0ca189d
Teddi <teddikao@gmail.com>
2025-08-30 17:29:44 +0800

    Add logic to enable query only when notebookId is not undefined

--- frontend/src/features/noteTaking/hooks/query/useNotebookInfoQuery.hooks.ts
+++ frontend/src/features/noteTaking/hooks/query/useNotebookInfoQuery.hooks.ts
@@ -1,5 +1,21 @@
-function useNotebookInfoQuery() {
-    
+import { useQuery } from "@tanstack/react-query";
+import { retrieveNotebook } from "../../utils/notebooks.services";
+
+function useNotebookInfoQuery(notebookId: number | undefined) {
+    return useQuery({
+        queryKey: ["notebookInfo", notebookId],
+        queryFn: async () => {
+            const fetchNotebookInfoResponse = await retrieveNotebook(notebookId!);
+            if (!fetchNotebookInfoResponse.success) {
+                throw new Error(fetchNotebookInfoResponse.error);
+            }
+
+            return fetchNotebookInfoResponse.retrievedNotebook;
+        },
+
+        staleTime: 2 * 1000 * 60,
+        enabled: !!notebookId,
+    });
 }
 
 export default useNotebookInfoQuery;
