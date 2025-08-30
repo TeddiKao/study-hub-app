3de45a8860ddeb49d723da747f13433b083f720c
Teddi <teddikao@gmail.com>
2025-08-30 15:16:05 +0800

    Create custom hook for managing notebook mutations

--- frontend/src/features/noteTaking/hooks/mutations/useNotebookMutations.hooks.ts
+++ frontend/src/features/noteTaking/hooks/mutations/useNotebookMutations.hooks.ts
@@ -1,5 +1,70 @@
+import { useQueryClient } from "@tanstack/react-query";
+import type {
+    CreateNotebookApiPayload,
+    EditNotebookApiPayload,
+} from "../../types/notebooks/notebookApi.types";
+import { createNotebook, editNotebook, deleteNotebook } from "../../utils/notebooks.services";
+import type { Notebooks } from "../../types/notebooks/notebookStore.types";
+
 function useNotebookMutations() {
-    
+    const queryClient = useQueryClient();
+
+    async function handleNotebookCreate(
+        createdNotebook: CreateNotebookApiPayload
+    ) {
+        const notebookCreateResponse = await createNotebook(createdNotebook);
+        if (!notebookCreateResponse.success) {
+            throw new Error(notebookCreateResponse.error);
+        }
+
+        queryClient.setQueryData(["notebooks"], (oldNotebooks: Notebooks) =>
+            oldNotebooks
+                ? [...oldNotebooks, notebookCreateResponse.createdNotebook]
+                : [notebookCreateResponse.createdNotebook]
+        );
+    }
+
+    async function handleNotebookEdit(
+        notebookId: number,
+        notebookData: EditNotebookApiPayload
+    ) {
+        const notebookEditResponse = await editNotebook(
+            notebookId,
+            notebookData
+        );
+        if (!notebookEditResponse.success) {
+            throw new Error(notebookEditResponse.error);
+        }
+
+        queryClient.setQueryData(["notebooks"], (oldNotebooks: Notebooks) =>
+            oldNotebooks
+                ? oldNotebooks.map((notebook) =>
+                      notebook.id === notebookId
+                          ? notebookEditResponse.editedNotebook
+                          : notebook
+                  )
+                : oldNotebooks
+        );
+    }
+
+    async function handleNotebookDelete(notebookId: number) {
+        const notebookDeleteResponse = await deleteNotebook(notebookId);
+        if (!notebookDeleteResponse.success) {
+            throw new Error(notebookDeleteResponse.error);
+        }
+
+        queryClient.setQueryData(["notebooks"], (oldNotebooks: Notebooks) =>
+            oldNotebooks
+                ? oldNotebooks.filter((notebook) => notebook.id !== notebookId)
+                : oldNotebooks
+        );
+    }
+
+    return {
+        handleNotebookCreate,
+        handleNotebookEdit,
+        handleNotebookDelete,
+    };
 }
 
 export default useNotebookMutations;
