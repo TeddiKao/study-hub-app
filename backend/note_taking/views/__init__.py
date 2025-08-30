8d7639dd49f8c649969804edd737036177d4eeb7
Teddi <teddikao@gmail.com>
2025-08-30 11:12:00 +0800

    Ensure that note count endpoint is re-exported

--- backend/note_taking/views/__init__.py
+++ backend/note_taking/views/__init__.py
@@ -1,4 +1,4 @@
-from .notebook_views import CreateNotebookEndpoint, EditNotebookEndpoint, DeleteNotebookEndpoint, FetchNotebooksEndpoint, RetrieveNotebookEndpoint
+from .notebook_views import CreateNotebookEndpoint, EditNotebookEndpoint, DeleteNotebookEndpoint, FetchNotebooksEndpoint, RetrieveNotebookEndpoint, GetNoteCountEndpoint
 from .note_views import FetchNotesEndpoint, CreateNoteEndpoint, DeleteNoteEndpoint, EditNoteEndpoint, RetrieveNoteEndpoint
 
-__all__ = ["CreateNotebookEndpoint", "EditNotebookEndpoint", "DeleteNotebookEndpoint", "FetchNotebooksEndpoint", "RetrieveNotebookEndpoint", "FetchNotesEndpoint", "CreateNoteEndpoint", "DeleteNoteEndpoint", "EditNoteEndpoint", "RetrieveNoteEndpoint"]
+__all__ = ["CreateNotebookEndpoint", "EditNotebookEndpoint", "DeleteNotebookEndpoint", "FetchNotebooksEndpoint", "RetrieveNotebookEndpoint", "FetchNotesEndpoint", "CreateNoteEndpoint", "DeleteNoteEndpoint", "EditNoteEndpoint", "RetrieveNoteEndpoint", "GetNoteCountEndpoint"]
