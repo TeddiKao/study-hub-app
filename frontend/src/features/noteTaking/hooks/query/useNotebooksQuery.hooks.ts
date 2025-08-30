272b85fedba313589c6164711b0aa8104f25c8d9
Teddi <teddikao@gmail.com>
2025-08-30 15:18:32 +0800

    Add querying logic for notebooks

--- frontend/src/features/noteTaking/hooks/query/useNotebooksQuery.hooks.ts
+++ frontend/src/features/noteTaking/hooks/query/useNotebooksQuery.hooks.ts
@@ -1,5 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchNotebooks } from "../../utils/notebooks.services";
+
 function useNotebooksQuery() {
-    
+    return useQuery({
+        queryKey: ["notebooks"],
+        queryFn: async () => {
+            const notebooksFetchResponse = await fetchNotebooks();
+
+            if (!notebooksFetchResponse.success) {
+                throw new Error(notebooksFetchResponse.error);
+            }
+
+            return notebooksFetchResponse.notebooks;
+        },
+
+        staleTime: 2 * 1000 * 60,
+    });
 }
 
 export default useNotebooksQuery;
