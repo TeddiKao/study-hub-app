b6046688d61e3495b974cf65da56d8e1d056a52c
Teddi <teddikao@gmail.com>
2025-08-30 11:53:00 +0800

    Add related_name to note model

--- backend/note_taking/models/note.py
+++ backend/note_taking/models/note.py
@@ -3,7 +3,7 @@ from django.db import models
 class Note(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(null=True, blank=True)
-    notebook = models.ForeignKey("note_taking.Notebook", on_delete=models.CASCADE, null=False, blank=False)
+    notebook = models.ForeignKey("note_taking.Notebook", related_name="notes", on_delete=models.CASCADE, null=False, blank=False)
 
     class Meta:
         constraints = [
