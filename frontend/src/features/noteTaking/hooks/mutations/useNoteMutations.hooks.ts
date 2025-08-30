0f047c05255f049857dd5d3f9d7cb69ceeecb27e
Teddi <teddikao@gmail.com>
2025-08-30 13:32:33 +0800

    Create custom hook to handle note mutations

--- frontend/src/features/noteTaking/hooks/mutations/useNoteMutations.hooks.ts
+++ frontend/src/features/noteTaking/hooks/mutations/useNoteMutations.hooks.ts
@@ -1,5 +1,76 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useNotesStore } from "../../stores/notes/notesStore.stores";
+import { isNullOrUndefined } from "@/shared/utils/types.utils";
+import { createNote, deleteNote, editNote } from "../../utils/notes.services";
+import type { RawNoteData } from "../../types/notes/notesApi.types";
+import type { Notes } from "../../types/notes/notesStore.types";
+
 function useNoteMutations() {
-    
+    const queryClient = useQueryClient();
+    const { currentNotebookId } = useNotesStore();
+
+    async function handleNoteCreate(createdNote: RawNoteData) {
+        if (isNullOrUndefined(currentNotebookId)) return;
+
+        const noteCreateResponse = await createNote(
+            createdNote,
+            currentNotebookId!
+        );
+        if (!noteCreateResponse.success) {
+            throw new Error(noteCreateResponse.error);
+        }
+
+        queryClient.setQueryData(
+            ["notes", currentNotebookId],
+            (oldNotes: Notes) => [...oldNotes, noteCreateResponse.createdNote]
+        );
+    }
+
+    async function handleNoteEdit(noteId: number, newNoteData: RawNoteData) {
+        if (isNullOrUndefined(currentNotebookId)) return;
+
+        const noteEditResponse = await editNote(
+            noteId,
+            {
+                name: newNoteData.name,
+                description: newNoteData.description,
+                notebookId: currentNotebookId!,
+            }
+        );
+
+        if (!noteEditResponse.success) {
+            throw new Error(noteEditResponse.error);
+        }
+
+        queryClient.setQueryData(
+            ["notes", currentNotebookId],
+            (oldNotes: Notes) =>
+                oldNotes.map((note) =>
+                    note.id === noteId ? noteEditResponse.editedNote : note
+                )
+        );
+    }
+
+    async function handleNoteDelete(noteId: number) {
+        if (isNullOrUndefined(currentNotebookId)) return;
+
+        const noteDeleteResponse = await deleteNote(noteId);
+        if (!noteDeleteResponse.success) {
+            throw new Error(noteDeleteResponse.error);
+        }
+
+        queryClient.setQueryData(
+            ["notes", currentNotebookId],
+            (oldNotes: Notes) =>
+                oldNotes.filter((note) => note.id !== noteId)
+        );
+    }
+
+    return {
+        handleNoteCreate,
+        handleNoteEdit,
+        handleNoteDelete,
+    };
 }
 
 export default useNoteMutations;
