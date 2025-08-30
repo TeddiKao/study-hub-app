7170a7bcdc203f0ad92e899fc43d3a022a44762a
Teddi <teddikao@gmail.com>
2025-08-30 17:48:27 +0800

    Improve logic for enabling query

--- frontend/src/features/noteTaking/hooks/query/useNotebookInfoQuery.hooks.ts
+++ frontend/src/features/noteTaking/hooks/query/useNotebookInfoQuery.hooks.ts
@@ -1,11 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { retrieveNotebook } from "../../utils/notebooks.services";
+import { isNullOrUndefined } from "@/shared/utils/types.utils";
 
 function useNotebookInfoQuery(notebookId: number | undefined) {
+    const isQueryEnabled =
+        !isNullOrUndefined(notebookId) && !Number.isNaN(notebookId);
+
     return useQuery({
         queryKey: ["notebookInfo", notebookId],
         queryFn: async () => {
-            const fetchNotebookInfoResponse = await retrieveNotebook(notebookId!);
+            const fetchNotebookInfoResponse = await retrieveNotebook(
+                notebookId!
+            );
             if (!fetchNotebookInfoResponse.success) {
                 throw new Error(fetchNotebookInfoResponse.error);
             }
@@ -14,7 +20,7 @@ function useNotebookInfoQuery(notebookId: number | undefined) {
         },
 
         staleTime: 2 * 1000 * 60,
-        enabled: !!notebookId,
+        enabled: isQueryEnabled,
     });
 }
 
