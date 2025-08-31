1512ecffc5e7840299f3478d721afb24c9fa906a
Teddi <teddikao@gmail.com>
2025-08-31 10:39:46 +0800

    Add basic editor configuration

--- frontend/src/features/noteTaking/editor/pages/NotesEditorPage.tsx
+++ frontend/src/features/noteTaking/editor/pages/NotesEditorPage.tsx
@@ -1,4 +1,13 @@
+import { useEditor } from "@tiptap/react";
+import Bold from "@tiptap/extension-bold";
+import Italic from "@tiptap/extension-italic";
+import Underline from "@tiptap/extension-underline";
+
 function NotesEditorPage() {
+    const editor = useEditor({
+        extensions: [Bold, Italic, Underline]
+    })
+
     return <div>NotesEditorPage</div>;
 }
 
