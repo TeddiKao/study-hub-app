4917e3b3fbdfc1dd8f666ec2109ee05c8de2d667
Teddi <teddikao@gmail.com>
2025-08-31 10:40:31 +0800

    Add basic editor content rendering

--- frontend/src/features/noteTaking/editor/pages/NotesEditorPage.tsx
+++ frontend/src/features/noteTaking/editor/pages/NotesEditorPage.tsx
@@ -1,14 +1,18 @@
-import { useEditor } from "@tiptap/react";
+import { EditorContent, useEditor } from "@tiptap/react";
 import Bold from "@tiptap/extension-bold";
 import Italic from "@tiptap/extension-italic";
 import Underline from "@tiptap/extension-underline";
 
 function NotesEditorPage() {
     const editor = useEditor({
-        extensions: [Bold, Italic, Underline]
-    })
+        extensions: [Bold, Italic, Underline],
+    });
 
-    return <div>NotesEditorPage</div>;
+    return (
+        <>
+            <EditorContent editor={editor} />
+        </>
+    );
 }
 
 export default NotesEditorPage;
