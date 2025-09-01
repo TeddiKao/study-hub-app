273b1e631359f317b42824990455f154e4cb3302
Teddi <teddikao@gmail.com>
2025-09-01 08:42:07 +0800

    Ensure that block orders can only be positive integers

--- backend/note_taking/models/block.py
+++ backend/note_taking/models/block.py
@@ -4,4 +4,4 @@ class Block(models.Model):
     block_type = models.CharField(max_length=50, null=False, blank=False)
     block_content = models.JSONField(null=False, blank=False, default=dict)
     note = models.ForeignKey("note_taking.Note", related_name="blocks", on_delete=models.CASCADE, null=False, blank=False)
-    block_order = models.IntegerField(null=False, blank=False)
+    block_order = models.PositiveIntegerField(null=False, blank=False)
