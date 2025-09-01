1181b5c2dfa728e9c1e446b30cc53c21fc37c155
Teddi <teddikao@gmail.com>
2025-09-01 15:09:52 +0800

    Add new block endpoints to __init__.py

--- backend/note_taking/views/__init__.py
+++ backend/note_taking/views/__init__.py
@@ -1,4 +1,20 @@
 from .notebook_views import CreateNotebookEndpoint, EditNotebookEndpoint, DeleteNotebookEndpoint, FetchNotebooksEndpoint, RetrieveNotebookEndpoint
 from .note_views import FetchNotesEndpoint, CreateNoteEndpoint, DeleteNoteEndpoint, EditNoteEndpoint, RetrieveNoteEndpoint
+from .block_views import FetchBlocksEndpoint, CreateBlockEndpoint, EditBlockEndpoint, DeleteBlockEndpoint
 
-__all__ = ["CreateNotebookEndpoint", "EditNotebookEndpoint", "DeleteNotebookEndpoint", "FetchNotebooksEndpoint", "RetrieveNotebookEndpoint", "FetchNotesEndpoint", "CreateNoteEndpoint", "DeleteNoteEndpoint", "EditNoteEndpoint", "RetrieveNoteEndpoint"]
+__all__ = [
+    "CreateNotebookEndpoint", 
+    "EditNotebookEndpoint", 
+    "DeleteNotebookEndpoint", 
+    "FetchNotebooksEndpoint", 
+    "RetrieveNotebookEndpoint", 
+    "FetchNotesEndpoint", 
+    "CreateNoteEndpoint", 
+    "DeleteNoteEndpoint", 
+    "EditNoteEndpoint", 
+    "RetrieveNoteEndpoint", 
+    "FetchBlocksEndpoint", 
+    "CreateBlockEndpoint", 
+    "EditBlockEndpoint", 
+    "DeleteBlockEndpoint"
+]
