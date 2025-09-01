d3aff5adec1222de3c3373a01ffe88f5f483d873
Teddi <teddikao@gmail.com>
2025-09-01 15:09:00 +0800

    Add endpoints for editing and delting blocks

--- backend/note_taking/views/block_views.py
+++ backend/note_taking/views/block_views.py
@@ -24,3 +24,38 @@ class FetchBlocksEndpoint(ListAPIView):
         queryset = Block.objects.filter(note=note_id, note__notebook__owner=self.request.user)
         
         return queryset
+
+
+class CreateBlockEndpoint(CreateAPIView):
+    queryset = Block.objects.all()
+    serializer_class = BlockSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        note = serializer.validated_data["note"]
+        if note.notebook.owner.id != self.request.user.id:
+            raise PermissionDenied("You do not have permission to create blocks in this note")
+
+class EditBlockEndpoint(UpdateAPIView):
+    serializer_class = BlockSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Block.objects.filter(note__notebook__owner=self.request.user)
+
+    def perform_update(self, serializer):
+        block = serializer.instance
+        if block.note.notebook.owner.id != self.request.user.id:
+            raise PermissionDenied("You do not have permission to edit blocks in this note")
+
+
+class DeleteBlockEndpoint(DestroyAPIView):
+    serializer_class = BlockSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Block.objects.filter(note__notebook__owner=self.request.user)
+
+    def perform_destroy(self, instance):
+        if instance.note.notebook.owner.id != self.request.user.id:
+            raise PermissionDenied("You do not have permission to delete blocks in this note")
