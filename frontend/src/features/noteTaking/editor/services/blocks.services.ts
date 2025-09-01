3aa22c3f8eb60e9e7a0d019d87541d8181358e62
Teddi <teddikao@gmail.com>
2025-09-01 16:10:11 +0800

    Create other service functions

--- frontend/src/features/noteTaking/editor/services/blocks.services.ts
+++ frontend/src/features/noteTaking/editor/services/blocks.services.ts
@@ -1,23 +1,54 @@
-import api from "@/app/api/api"
+import api from "@/app/api/api";
 import { BLOCKS_BASE } from "@/app/api/api.constants";
-import type { ApiErrorResponse, ApiSuccessResponse } from "@/shared/types/api.types";
+import type {
+    ApiErrorResponse,
+    ApiSuccessResponse,
+} from "@/shared/types/api.types";
 import { isAxiosError } from "axios";
 
 interface Block {
     id: number;
-    block_type: string;
-    block_content: string;
+    blockType: string;
+    blockContent: string;
     note: number;
-    block_order: number;
+    blockOrder: number;
+}
+
+interface RawBlockData {
+    blockType: string;
+    blockContent: string;
+    blockOrder: number;
 }
 
 interface FetchBlocksSuccess extends ApiSuccessResponse {
     blocks: Block[];
 }
 
-async function fetchBlocks(noteId: number): Promise<FetchBlocksSuccess | ApiErrorResponse> {
+interface CreateBlockSuccess extends ApiSuccessResponse {
+    createdBlock: Block;
+}
+
+interface RetrieveBlockSuccess extends ApiSuccessResponse {
+    block: Block;
+}
+
+interface EditBlockSuccess extends ApiSuccessResponse {
+    block: Block;
+}
+
+interface DeleteBlockSuccess extends ApiSuccessResponse {
+    message: string;
+}
+
+async function fetchBlocks(
+    noteId: number
+): Promise<FetchBlocksSuccess | ApiErrorResponse> {
     try {
-        const response = await api.get(`${BLOCKS_BASE}/note/${noteId}/blocks/`);
+        const response = await api.get(`${BLOCKS_BASE}/`, {
+            params: {
+                noteId: noteId,
+            },
+        });
 
         return {
             success: true,
@@ -39,27 +70,112 @@ async function fetchBlocks(noteId: number): Promise<FetchBlocksSuccess | ApiErro
     }
 }
 
-async function createBlock() {
+async function createBlock(
+    noteId: number,
+    blockData: RawBlockData
+): Promise<CreateBlockSuccess | ApiErrorResponse> {
+    try {
+        const response = await api.post(`${BLOCKS_BASE}/create/`, {
+            blockType: blockData.blockType,
+            blockContent: blockData.blockContent,
+            blockOrder: blockData.blockOrder,
+            noteId: noteId,
+        });
 
+        return {
+            success: true,
+            message: "Successfully created block",
+            createdBlock: response.data,
+        };
+    } catch (error) {
+        if (!isAxiosError(error)) {
+            return {
+                success: false,
+                error: "Failed to create block",
+            };
+        }
+
+        return {
+            success: false,
+            error: error.response?.data?.error ?? "Failed to create block",
+        };
+    }
 }
 
-async function retrieveBlock() {
+async function retrieveBlock(
+    blockId: number
+): Promise<RetrieveBlockSuccess | ApiErrorResponse> {
+    try {
+        const response = await api.get(`${BLOCKS_BASE}/block/${blockId}/`);
 
+        return {
+            success: true,
+            message: "Successfully retrieved block",
+            block: response.data,
+        };
+    } catch (error) {
+        if (!isAxiosError(error)) {
+            return {
+                success: false,
+                error: "Failed to retrieve block",
+            };
+        }
+
+        return {
+            success: false,
+            error: error.response?.data?.error ?? "Failed to retrieve block",
+        };
+    }
 }
 
+async function editBlock(blockId: number, blockData: RawBlockData) {
+    try {
+        const response = await api.put(
+            `${BLOCKS_BASE}/block/${blockId}/edit/`,
+            blockData
+        );
 
-async function editBlock() {
+        return {
+            success: true,
+            message: "Successfully edited block",
+            block: response.data,
+        };
+    } catch (error) {
+        if (!isAxiosError(error)) {
+            return {
+                success: false,
+                error: "Failed to edit block",
+            };
+        }
 
+        return {
+            success: false,
+            error: error.response?.data?.error ?? "Failed to edit block",
+        };
+    }
 }
 
-async function deleteBlock() {
+async function deleteBlock(blockId: number) {
+    try {
+        await api.delete(`${BLOCKS_BASE}/block/${blockId}/delete/`);
 
+        return {
+            success: true,
+            message: "Block deleted successfully",
+        };
+    } catch (error) {
+        if (!isAxiosError(error)) {
+            return {
+                success: false,
+                error: "Failed to delete block",
+            };
+        }
+
+        return {
+            success: false,
+            error: error.response?.data?.error ?? "Failed to delete block",
+        };
+    }
 }
 
-export {
-    fetchBlocks,
-    createBlock,
-    retrieveBlock,
-    editBlock,
-    deleteBlock,
-}
+export { fetchBlocks, createBlock, retrieveBlock, editBlock, deleteBlock };
