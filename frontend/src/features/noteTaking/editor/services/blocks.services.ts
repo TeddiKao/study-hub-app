8d653bc17ea429a3ed6f13fb48f941e3af7f3ccf
Teddi <teddikao@gmail.com>
2025-09-01 15:54:06 +0800

    Create placeholder block service functions

---
+++ frontend/src/features/noteTaking/editor/services/blocks.services.ts
@@ -0,0 +1,28 @@
+async function fetchBlocks() {
+
+}
+
+async function createBlock() {
+
+}
+
+async function retrieveBlock() {
+
+}
+
+
+async function editBlock() {
+
+}
+
+async function deleteBlock() {
+
+}
+
+export {
+    fetchBlocks,
+    createBlock,
+    retrieveBlock,
+    editBlock,
+    deleteBlock,
+}
