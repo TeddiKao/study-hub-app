3a156515984b21ff66a76a00730939a88fcb4309
Teddi <teddikao@gmail.com>
2025-09-02 20:34:20 +0800

    Rename field to prevent conflicts with SQL keywords

--- backend/note_taking/models/block.py
+++ backend/note_taking/models/block.py
@@ -4,13 +4,13 @@ class Block(models.Model):
     type = models.CharField(max_length=50, null=False, blank=False)
     content = models.JSONField(null=False, blank=False, default=dict)
     note = models.ForeignKey("note_taking.Note", related_name="blocks", on_delete=models.CASCADE, null=False, blank=False)
-    order = models.PositiveIntegerField(null=False, blank=False)
+    position = models.PositiveIntegerField(null=False, blank=False)
 
     class Meta:
-        ordering = ("note", "order")
+        ordering = ("note", "position")
         constraints = [
             models.UniqueConstraint(
-                fields=("note", "order"),
-                name="unique_note_block_order",
+                fields=("note", "position"),
+                name="unique_note_block_position",
             )
         ]
