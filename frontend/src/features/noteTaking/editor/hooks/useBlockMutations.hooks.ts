ad26de7355b7baad22a3a1fcf0dee3306ad533aa
Teddi <teddikao@gmail.com>
2025-09-02 19:58:22 +0800

    Create function for creating block

--- frontend/src/features/noteTaking/editor/hooks/useBlockMutations.hooks.ts
+++ frontend/src/features/noteTaking/editor/hooks/useBlockMutations.hooks.ts
@@ -1,14 +1,30 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useBlocksStore } from "../stores/blocks.stores";
+import type { RawBlockData } from "../types/blocksApi.types";
+import { createBlock } from "../services/blocks.services";
+import type { Blocks } from "../types/blockSchema.types";
 
 function useBlockMutations() {
     const queryClient = useQueryClient();
     const { currentNoteId } = useBlocksStore();
 
-    async function handleBlockCreate() {
+    async function handleBlockCreate(blockData: RawBlockData) {
         if (!currentNoteId) {
             return;
         }
+
+        const createBlockResponse = await createBlock(currentNoteId, blockData);
+        if (!createBlockResponse.success) {
+            throw new Error(createBlockResponse.error);
+        }
+
+        queryClient.setQueryData(["blocks", currentNoteId], (oldBlocks: Blocks) =>
+            oldBlocks ? [...oldBlocks, createBlockResponse.createdBlock] : [createBlockResponse.createdBlock]
+        );
+
+        await queryClient.invalidateQueries({
+            queryKey: ["blocks", currentNoteId],
+        });
     }
 
     async function handleBlockUpdate() {
