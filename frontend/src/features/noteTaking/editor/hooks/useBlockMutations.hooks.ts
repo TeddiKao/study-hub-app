6468b41bc4c873ee5ee17de2ea0eba040ccc4c6d
Teddi <teddikao@gmail.com>
2025-09-02 19:52:54 +0800

    Add basic logic to all functions

--- frontend/src/features/noteTaking/editor/hooks/useBlockMutations.hooks.ts
+++ frontend/src/features/noteTaking/editor/hooks/useBlockMutations.hooks.ts
@@ -1,14 +1,26 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useBlocksStore } from "../stores/blocks.stores";
+
 function useBlockMutations() {
+    const queryClient = useQueryClient();
+    const { currentNoteId } = useBlocksStore();
+
     async function handleBlockCreate() {
-        
+        if (!currentNoteId) {
+            return;
+        }
     }
 
     async function handleBlockUpdate() {
-        
+        if (!currentNoteId) {
+            return;
+        }
     }
 
     async function handleBlockDelete() {
-        
+        if (!currentNoteId) {
+            return;
+        }
     }
 
     return {
