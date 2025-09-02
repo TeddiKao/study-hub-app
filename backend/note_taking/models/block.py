5b044404294cbc8ad67db262318d92dc2ba46312
Teddi <teddikao@gmail.com>
2025-09-02 20:21:49 +0800

    Rename fields to match tiptap schema

--- backend/note_taking/models/block.py
+++ backend/note_taking/models/block.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class Block(models.Model):
-    block_type = models.CharField(max_length=50, null=False, blank=False)
-    block_content = models.JSONField(null=False, blank=False, default=dict)
+    type = models.CharField(max_length=50, null=False, blank=False)
+    content = models.JSONField(null=False, blank=False, default=dict)
     note = models.ForeignKey("note_taking.Note", related_name="blocks", on_delete=models.CASCADE, null=False, blank=False)
-    block_order = models.PositiveIntegerField(null=False, blank=False)
+    order = models.PositiveIntegerField(null=False, blank=False)
 
     class Meta:
-        ordering = ("note", "block_order")
+        ordering = ("note", "order")
         constraints = [
             models.UniqueConstraint(
-                fields=("note", "block_order"),
+                fields=("note", "order"),
                 name="unique_note_block_order",
             )
         ]
