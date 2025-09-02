4064d86464c6bf91c5fd9887cfcaa10ae65b61f1
Teddi <teddikao@gmail.com>
2025-09-02 19:52:01 +0800

    Create placeholder functions for creating, updating and deleting block

--- frontend/src/features/noteTaking/editor/hooks/useBlockMutations.hooks.ts
+++ frontend/src/features/noteTaking/editor/hooks/useBlockMutations.hooks.ts
@@ -1,5 +1,21 @@
 function useBlockMutations() {
-    
+    async function handleBlockCreate() {
+        
+    }
+
+    async function handleBlockUpdate() {
+        
+    }
+
+    async function handleBlockDelete() {
+        
+    }
+
+    return {
+        handleBlockCreate,
+        handleBlockUpdate,
+        handleBlockDelete,
+    };
 }
 
 export default useBlockMutations;
