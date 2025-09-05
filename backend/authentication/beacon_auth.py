7f02da7f482dce4af49d228fe36e6590b4f4ff52
Teddi <teddikao@gmail.com>
2025-09-05 13:37:54 +0800

    Create custom auth class for navigator beacon requests

---
+++ backend/authentication/beacon_auth.py
@@ -0,0 +1,23 @@
+import json
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+class BeaconAuthMiddleware(JWTAuthentication):
+    def authenticate(self, request: Request) -> Optional[tuple[AuthUser, Token]]:
+        auth = super().authenticate(request)
+
+        if auth is not None:
+            return auth
+
+        if request.method == "POST":
+            try:
+                data = json.loads(request.body)
+                token = data.get("token")
+                if token:
+                    validated_token = self.get_validated_token(token)
+                    user = self.get_user(validated_token)
+                    return (user, validated_token)
+            except Exception:
+                pass
+
+        return None
