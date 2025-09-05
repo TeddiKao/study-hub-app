a7ad594d34b3638f029e95d3b0b8d93729c3b11b
Teddi <teddikao@gmail.com>
2025-09-05 15:03:24 +0800

    Create serializer for a list of blocks

---
+++ backend/note_taking/serializers/block_list_serializer.py
@@ -0,0 +1,29 @@
+from rest_framework.serializers import ListSerializer
+
+from ..models import Block
+
+class BlockListSerializer(ListSerializer):
+    def create(self, validated_data):
+        blocks = []
+        for item in validated_data:
+            if not item.get("id"):
+                blocks.append(Block.objects.create(**item))
+
+        return blocks
+
+    def update(self, instance, validated_data):
+        block_mapping = {}
+        for block in instance:
+            block_mapping[block.id] = block
+
+        for item in validated_data:
+            block_id = item.get("id")
+            if block_id and block_id in block_mapping:
+                block = block_mapping[block_id]
+                
+                for attr, value in item.items():
+                    setattr(block, attr, value)
+                
+                block.save()
+        
+        return instance
