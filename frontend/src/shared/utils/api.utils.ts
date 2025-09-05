b206bfc9b3e94c55b2f877ccd32d6c069eb5a03c
Teddi <teddikao@gmail.com>
2025-09-05 12:52:17 +0800

    Create helper function for sending beacon requests for requests that occur during window unload

---
+++ frontend/src/shared/utils/api.utils.ts
@@ -0,0 +1,12 @@
+function sendBeacon(url: string, data: any) {
+    try {
+        const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+        navigator.sendBeacon(url, blob);
+
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+export { sendBeacon }
