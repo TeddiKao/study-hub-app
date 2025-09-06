6a59e124080c23a41591c9893f24dc47c3ee5ed2
Teddi <teddikao@gmail.com>
2025-09-06 12:43:32 +0800

    Create separate serializer class for list of blocks and single blocks

--- backend/note_taking/serializers/bulk_block_serializer.py
+++ backend/note_taking/serializers/bulk_block_serializer.py
@@ -1,16 +1,17 @@
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
-from ..models import Block
-from ..models import Note
-from .note_serializer import NoteSerializer
+from ..models import Block, Note
 
-class BlockSerializer(ModelSerializer):
+from .note_serializer import NoteSerializer
+from .block_list_serializer import BlockListSerializer
+
+class BulkBlockSerializer(ModelSerializer):
+    block_id = serializers.IntegerField(required=False, read_only=False)
     note = NoteSerializer(read_only=True)
     note_id = PrimaryKeyRelatedField(
         queryset=Note.objects.none(),
         write_only=True,
-        source="note"
     )
 
     def __init__(self, *args, **kwargs):
@@ -22,4 +23,7 @@ class BlockSerializer(ModelSerializer):
 
     class Meta:
         model = Block
-        fields = ["id", "type", "content", "note", "note_id", "position"]
+        fields = ["id", "block_id", "type", "content", "note", "note_id", "position"]
+        read_only_fields = ["id"]
+        list_serializer_class = BlockListSerializer
+    
