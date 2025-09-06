a0e37d59499667103f7e9b0500cd9ebe6592f1b0
Teddi <teddikao@gmail.com>
2025-09-06 10:14:50 +0800

    Add additional type checks for navigator send beacon

--- frontend/src/shared/utils/api.utils.ts
+++ frontend/src/shared/utils/api.utils.ts
@@ -1,4 +1,5 @@
 import { useAuthTokensStore } from "@/features/auth/stores/authTokens.stores";
+import { isNullOrUndefined } from "./types.utils";
 
 function sendBeacon(url: string, data: any) {
     const accessToken = useAuthTokensStore.getState().accessToken;
@@ -9,13 +10,14 @@ function sendBeacon(url: string, data: any) {
     }
 
     try {
+        if (isNullOrUndefined(navigator)) return false;
+        if (!("sendBeacon" in navigator)) return false;
+
         const blob = new Blob([JSON.stringify(blobData)], {
             type: "application/json",
         });
 
-        navigator.sendBeacon(url, blob);
-
-        return true;
+        return navigator.sendBeacon(url, blob);
     } catch (error) {
         console.error(error);
         return false;
