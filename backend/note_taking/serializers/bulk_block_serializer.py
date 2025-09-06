f51d1993cc4329b76163506520ab3a8fc1e1625c
Teddi <teddikao@gmail.com>
2025-09-06 14:04:06 +0800

    Add missing PrimaryKeyRelatedField import

--- backend/note_taking/serializers/bulk_block_serializer.py
+++ backend/note_taking/serializers/bulk_block_serializer.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from rest_framework import serializers
 
 from ..models import Block, Note
