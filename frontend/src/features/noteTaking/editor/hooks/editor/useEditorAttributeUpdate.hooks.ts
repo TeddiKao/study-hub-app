590e2964339173a6ce6cea8b6baac73aacd7f4fe
Teddi <teddikao@gmail.com>
2025-09-07 18:23:43 +0800

    Create custom hook for attribute updating logic

---
+++ frontend/src/features/noteTaking/editor/hooks/editor/useEditorAttributeUpdate.hooks.ts
@@ -0,0 +1,49 @@
+import type { Editor } from "@tiptap/react";
+import useBlocksQuery from "../blocks/useBlocksQuery.hooks";
+import { isNullOrUndefined } from "@/shared/utils/types.utils";
+import { useEffect } from "react";
+import { getNodePositionById, getNodePositionByIndex } from "../../utils/nodes.utils";
+
+function useEditorAttributeUpdate(editor: Editor) {
+    const { data: blocks } = useBlocksQuery();
+
+    useEffect(() => {
+        if (!blocks) return;
+        if (!editor) return;
+
+        blocks.forEach((block) => {
+            const position = getNodePositionById(editor, block.attrs.id);
+            if (isNullOrUndefined(position)) {
+                const positionByIndex = getNodePositionByIndex(
+                    editor,
+                    block.attrs.position
+                );
+                if (isNullOrUndefined(positionByIndex)) return;
+
+                const nodeType = editor.schema.nodes[block.type];
+                if (!nodeType) return;
+
+                editor.commands.command(({ tr: transaction }) => {
+                    transaction.setNodeMarkup(
+                        positionByIndex!,
+                        nodeType,
+                        block.attrs
+                    );
+                    return true;
+                });
+
+                return;
+            }
+
+            const nodeType = editor.schema.nodes[block.type];
+            if (!nodeType) return;
+
+            editor.commands.command(({ tr: transaction }) => {
+                transaction.setNodeMarkup(position!, nodeType, block.attrs);
+                return true;
+            });
+        });
+    }, [blocks, editor]);
+}
+
+export default useEditorAttributeUpdate;
