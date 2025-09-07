acd3f8d59c56b45c61238dd5b086160969269a4f
Teddi <teddikao@gmail.com>
2025-09-07 11:02:17 +0800

    Enhance ID validation and owner scoping

--- backend/note_taking/views/block_views.py
+++ backend/note_taking/views/block_views.py
@@ -83,8 +83,21 @@ class BulkUpdateBlocksEndpoint(APIView):
             note__notebook__owner=self.request.user
         ))
 
+        instances_by_id = {}
+        for block in blocks_queryset:
+            instances_by_id[block.id] = block
+
+        instances = []
+        for id in block_ids:
+            try:
+                instances.append(instances_by_id[id])
+            except KeyError:
+                raise PermissionDenied(
+                    "One or more blocks do not exist, or you may lack access to them"
+                )
+
         serializer = BulkBlockSerializer(
-            instance=blocks_queryset,
+            instance=instances,
             data=blocks_data,
             many=True,
             context={"request": request}
