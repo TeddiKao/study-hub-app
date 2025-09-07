19f077b194a404828d79c1c8b83c6eaaeebb4e6a
Teddi <teddikao@gmail.com>
2025-09-07 14:46:31 +0800

    Add logic for computing created paragraphs to be sent to the server

--- frontend/src/features/noteTaking/editor/hooks/editor/useNotesEditor.hooks.ts
+++ frontend/src/features/noteTaking/editor/hooks/editor/useNotesEditor.hooks.ts
@@ -1,5 +1,5 @@
 import { Document } from "@tiptap/extension-document";
-import { useEditor } from "@tiptap/react";
+import { useEditor, type EditorEvents } from "@tiptap/react";
 import { Title } from "../../extensions/Title.node";
 import { NoteEditorParagraph } from "../../extensions/Paragraph.node";
 import { Bold } from "@tiptap/extension-bold";
@@ -8,8 +8,33 @@ import { Underline } from "@tiptap/extension-underline";
 import { Heading } from "@tiptap/extension-heading";
 import { Placeholder } from "@tiptap/extensions";
 import { Text } from "@tiptap/extension-text";
+import type { RawBlockData } from "../../types/blocksApi.types";
+import { isNullOrUndefined } from "@/shared/utils/types.utils";
 
 function useNotesEditor() {
+    function handleOnUpdate({ editor }: EditorEvents["update"]) {
+        if (!editor) return;
+        if (editor.isEmpty) return;
+
+        const createdParagraphs: RawBlockData[] = [];
+
+        editor.state.doc.descendants((node, pos) => {
+            if (node.type.name === "note_editor_paragraph") {
+                const id = node.attrs.id;
+
+                if (isNullOrUndefined(id)) {
+                    createdParagraphs.push({
+                        type: "note_editor_paragraph",
+                        content: node.content.toJSON(),
+                        position: pos,
+                    })
+                }
+            }
+        });
+
+        console.log(createdParagraphs);
+    }
+
     return useEditor({
         extensions: [
             Document,
@@ -32,6 +57,7 @@ function useNotesEditor() {
                 },
             }),
         ],
+        onUpdate: handleOnUpdate,
     });
 }
 
