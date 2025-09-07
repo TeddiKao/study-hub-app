7e1a7ceb3979d06675b55e9d7d1b4e0b876aa15b
Teddi <teddikao@gmail.com>
2025-09-07 11:42:09 +0800

    Create hook for window unload saving logic

---
+++ frontend/src/features/noteTaking/editor/hooks/editor/useWindowUnloadSave.hooks.ts
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import type { Editor } from "@tiptap/react";
+import { parseSerializedBlocks } from "../../utils/blocks.utils";
+import { BACKEND_BASE, BLOCKS_BASE } from "@/app/api/api.constants";
+import { sendBeacon } from "@/shared/utils/api.utils";
+import type { TiptapSerializedBlocks } from "../../types/blockSchema.types";
+
+function useWindowUnloadSave(editor: Editor | null, noteId: number) {
+    useEffect(() => {
+        const onBeforeUnload = () => {
+            if (!editor) return;
+            if (editor.isEmpty) return;
+
+            const formattedBlocks = parseSerializedBlocks(
+                editor.getJSON().content as TiptapSerializedBlocks
+            );
+
+            sendBeacon(`${BACKEND_BASE}/${BLOCKS_BASE}/bulk-update/`, {
+                blocks: formattedBlocks,
+            });
+        };
+
+        window.addEventListener("beforeunload", onBeforeUnload);
+
+        return () => {
+            window.removeEventListener("beforeunload", onBeforeUnload);
+        };
+    }, [editor, noteId]);
+}
+
+export default useWindowUnloadSave;
