9ebd1df84f6aaec181d97dff3aa08bfe785ddca1
Teddi <teddikao@gmail.com>
2025-09-08 12:36:10 +0800

    Create utility function for normalizing list of IDs

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,2 +1,28 @@
-def are_items_unique(items: list[any]) -> bool:
-    return len(items) == len(set(items))
+from typing import Any
+from django.core.exceptions import ValidationError
+
+def normalize_ids(ids: list[Any]) -> list[int]:
+    normalized_ids = []
+    seen_ids = set()
+
+    for index, raw in enumerate(ids):
+        try:
+            id = int(raw)
+        except (ValueError, TypeError):
+            raise ValidationError({
+                "ids": {
+                    index: "Must be an integer"
+                }
+            })
+
+        if id in seen_ids:
+            raise ValidationError({
+                "ids": {
+                    index: "Duplicate ID"
+                }
+            })
+
+        seen_ids.add(id)
+        normalized_ids.append(id)
+
+    return normalized_ids
