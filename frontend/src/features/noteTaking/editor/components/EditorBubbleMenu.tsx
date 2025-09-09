0e5a05f2bbd9b8560d45e40d7e61df4ba9b631ec
Teddi <teddikao@gmail.com>
2025-09-09 18:29:03 +0800

    Add functionality for bubble menu options

--- frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
+++ frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
@@ -1,6 +1,7 @@
 import { BubbleMenu } from "@tiptap/react/menus";
 import type { Editor } from "@tiptap/react";
 import { Bold, Italic, Underline } from "lucide-react";
+import { toggleBold, toggleItalic, toggleUnderline } from "../utils/marks.utils";
 
 interface EditorBubbleMenuProps {
     editor: Editor | null;
@@ -19,18 +20,21 @@ function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
         >
             <div className="flex flex-row px-2 py-1 gap-1 bg-white shadow-md rounded-md">
                 <button
+                    onClick={() => toggleBold(editor)}
                     type="button"
                     className="hover:cursor-pointer hover:bg-gray-300 bg-white border-none outline-none p-1 rounded-md"
                 >
                     <Bold size={20} strokeWidth={1.5} />
                 </button>
                 <button
+                    onClick={() => toggleItalic(editor)}
                     type="button"
                     className="hover:cursor-pointer hover:bg-gray-300 bg-white border-none outline-none p-1 rounded-md"
                 >
                     <Italic size={20} strokeWidth={1.5} />
                 </button>
                 <button
+                    onClick={() => toggleUnderline(editor)}
                     type="button"
                     className="hover:cursor-pointer hover:bg-gray-300 bg-white border-none outline-none p-1 rounded-md"
                 >
