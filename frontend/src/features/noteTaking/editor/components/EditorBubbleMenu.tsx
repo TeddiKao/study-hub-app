70411ee43e9e46db0a3fc413e64e10ad947ed38e
Teddi <teddikao@gmail.com>
2025-09-09 18:01:11 +0800

    Add background color hover styling

--- frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
+++ frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
@@ -1,30 +1,42 @@
-import { BubbleMenu } from "@tiptap/react/menus"
-import type { Editor } from "@tiptap/react"
-import { Bold, Italic, Underline } from "lucide-react"
+import { BubbleMenu } from "@tiptap/react/menus";
+import type { Editor } from "@tiptap/react";
+import { Bold, Italic, Underline } from "lucide-react";
 
 interface EditorBubbleMenuProps {
-    editor: Editor
+    editor: Editor;
 }
 
 function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
     return (
-        <BubbleMenu editor={editor} options={{
-            placement: "bottom",
-            offset: 8
-        }}>
-            <div className="flex flex-row p-1 gap-1 bg-white shadow-md rounded-md">
-                <button type="button" className="hover:cursor-pointer bg-white border-none outline-none">
+        <BubbleMenu
+            editor={editor}
+            options={{
+                placement: "bottom",
+                offset: 8,
+            }}
+        >
+            <div className="flex flex-row px-2 py-1 gap-1 bg-white shadow-md rounded-md">
+                <button
+                    type="button"
+                    className="hover:cursor-pointer hover:bg-gray-300 bg-white border-none outline-none p-1"
+                >
                     <Bold strokeWidth={1.5} />
                 </button>
-                <button type="button" className="hover:cursor-pointer bg-white border-none outline-none">
+                <button
+                    type="button"
+                    className="hover:cursor-pointer hover:bg-gray-300 bg-white border-none outline-none p-1"
+                >
                     <Italic strokeWidth={1.5} />
                 </button>
-                <button type="button" className="hover:cursor-pointer bg-white border-none outline-none">
+                <button
+                    type="button"
+                    className="hover:cursor-pointer hover:bg-gray-300 bg-white border-none outline-none p-1"
+                >
                     <Underline strokeWidth={1.5} />
                 </button>
             </div>
         </BubbleMenu>
-    )
+    );
 }
 
-export default EditorBubbleMenu
+export default EditorBubbleMenu;
