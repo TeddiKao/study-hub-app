65950ad646c1d20965f96891e2460a7c52d926f7
Teddi <teddikao@gmail.com>
2025-09-09 20:18:31 +0800

    Modify utility function for toggling heading such that it works with custom extensions

--- frontend/src/features/noteTaking/editor/utils/blocks.utils.ts
+++ frontend/src/features/noteTaking/editor/utils/blocks.utils.ts
@@ -4,14 +4,15 @@ import type {
     TiptapSerializedBlock,
     TiptapSerializedBlocks,
 } from "../types/blockSchema.types";
-import type { Editor} from "@tiptap/react";
-import type { Level } from "@tiptap/extension-heading";
+import type { Editor } from "@tiptap/react";
 
-function parseSerializedBlock(block: TiptapSerializedBlock): BlockUpdateRequest {
+function parseSerializedBlock(
+    block: TiptapSerializedBlock
+): BlockUpdateRequest {
     if (!block.attrs) {
         throw new Error("Block attrs is missing");
     }
-    
+
     if (isNullOrUndefined(block.attrs.id)) {
         throw new Error("Block ID is missing");
     }
@@ -24,14 +25,17 @@ function parseSerializedBlock(block: TiptapSerializedBlock): BlockUpdateRequest 
         throw new Error("Block position is missing");
     }
 
-    if (Number.isNaN(block.attrs.position) || !Number.isFinite(block.attrs.position)) {
+    if (
+        Number.isNaN(block.attrs.position) ||
+        !Number.isFinite(block.attrs.position)
+    ) {
         throw new Error("Block position is not a number");
     }
 
     if (isNullOrUndefined(block.attrs.note?.id)) {
         throw new Error("Block note is missing");
     }
-    
+
     const {
         type,
         content,
@@ -45,17 +49,19 @@ function parseSerializedBlock(block: TiptapSerializedBlock): BlockUpdateRequest 
         content: content,
         position: position,
         noteId: note.id,
-    }
+    };
 }
 
-function parseSerializedBlocks(
-    tiptapSerializedBlocks: TiptapSerializedBlocks
-) {
+function parseSerializedBlocks(tiptapSerializedBlocks: TiptapSerializedBlocks) {
     return tiptapSerializedBlocks.map((block) => parseSerializedBlock(block));
 }
 
 function toggleHeading(editor: Editor, level: number) {
-    editor.chain().focus().toggleHeading({ level: level as Level }).run();
+    editor
+        .chain()
+        .focus()
+        .toggleNode("note_editor_paragraph", "note_editor_heading", { level })
+        .run();
 }
 
-export { parseSerializedBlocks, toggleHeading }
+export { parseSerializedBlocks, toggleHeading };
