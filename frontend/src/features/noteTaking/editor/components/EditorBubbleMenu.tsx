6aac8b654b08344e04725e1dda973a4ac62084ab
Teddi <teddikao@gmail.com>
2025-09-09 17:51:36 +0800

    Add basic markup for bold italic and underline buttons

--- frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
+++ frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
@@ -1,5 +1,7 @@
 import { BubbleMenu } from "@tiptap/react/menus"
 import type { Editor } from "@tiptap/react"
+import { Button } from "@/components/ui/button"
+import { Bold, Italic, Underline } from "lucide-react"
 
 function EditorBubbleMenu(editor: Editor) {
     return (
@@ -7,7 +9,17 @@ function EditorBubbleMenu(editor: Editor) {
             placement: "bottom",
             offset: 8
         }}>
-            
+            <div className="flex flex-row">
+                <Button type="button" className="hover:cursor-pointer">
+                    <Bold />
+                </Button>
+                <Button type="button" className="hover:cursor-pointer">
+                    <Italic />
+                </Button>
+                <Button type="button" className="hover:cursor-pointer">
+                    <Underline />
+                </Button>
+            </div>
         </BubbleMenu>
     )
 }
