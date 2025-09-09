3086b05c1fc5c83ce8d194015c4d63ce0aa9f555
Teddi <teddikao@gmail.com>
2025-09-09 17:50:04 +0800

    Add basic configurations for bubble menu

--- frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
+++ frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
@@ -1,5 +1,15 @@
-function EditorBubbleMenu() {
+import { BubbleMenu } from "@tiptap/react/menus"
+import type { Editor } from "@tiptap/react"
 
+function EditorBubbleMenu(editor: Editor) {
+    return (
+        <BubbleMenu editor={editor} options={{
+            placement: "bottom",
+            offset: 8
+        }}>
+            
+        </BubbleMenu>
+    )
 }
 
 export default EditorBubbleMenu
