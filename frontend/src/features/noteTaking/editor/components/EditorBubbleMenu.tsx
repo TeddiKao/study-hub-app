12b00b2480c31bb071073f6483febae725300222
Teddi <teddikao@gmail.com>
2025-09-09 18:40:44 +0800

    Add MarkButton component for better reusability and reduced duplication

--- frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
+++ frontend/src/features/noteTaking/editor/components/EditorBubbleMenu.tsx
@@ -2,9 +2,54 @@ import { BubbleMenu } from "@tiptap/react/menus";
 import type { Editor } from "@tiptap/react";
 import { Bold, Italic, Underline } from "lucide-react";
 import { toggleBold, toggleItalic, toggleUnderline } from "../utils/marks.utils";
+import clsx from "clsx";
 
 interface EditorBubbleMenuProps {
     editor: Editor | null;
+}
+
+interface MarkButtonProps {
+    editor: Editor | null;
+    markName: string;
+    isActive: boolean;
+}
+
+function MarkButton({ editor, markName, isActive }: MarkButtonProps) {
+    if (!editor) return null;
+
+    function getMarkIcon() {
+        switch (markName) {
+            case "bold":
+                return <Bold size={20} strokeWidth={1.5} className={getIconColorClass()} />;
+
+            case "italic":
+                return <Italic size={20} strokeWidth={1.5} className={getIconColorClass()} />;
+
+            case "underline":
+                return <Underline size={20} strokeWidth={1.5} className={getIconColorClass()} />;
+
+            default:
+                return null;
+        }
+    }
+
+    function getActiveClass() {
+        return isActive ? "bg-sky-500 hover:bg-sky-700" : "bg-white hover:bg-gray-300";
+    }
+
+    function getIconColorClass() {
+        return isActive ? "fill-white" : "fill-gray-500";
+    }
+
+    return (
+        <button
+            onClick={() => toggleBold(editor)}
+            type="button"
+            className={clsx("border-none outline-none p-1 rounded-md", getActiveClass())}
+        >
+            {getMarkIcon()}
+        </button>
+    );
 }
 
 function EditorBubbleMenu({ editor }: EditorBubbleMenuProps) {
