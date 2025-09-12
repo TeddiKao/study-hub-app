aca090d3e860ab45cdc2d11184516a89358516fb
Teddi <teddikao@gmail.com>
2025-09-12 20:09:56 +0800

    Add relative_position field to allow clients to insert fields relative to a block's position

--- backend/note_taking/serializers/bulk_block_serializer.py
+++ backend/note_taking/serializers/bulk_block_serializer.py
@@ -17,6 +17,7 @@ class BulkBlockSerializer(ModelSerializer):
         write_only=True,
     )
     position = serializers.IntegerField(required=False, read_only=False)
+    relative_position = serializers.DictField(required=False, read_only=False)
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
@@ -37,7 +38,7 @@ class BulkBlockSerializer(ModelSerializer):
 
     class Meta:
         model = Block
-        fields = ["id", "block_id", "type", "content", "note", "note_id", "position", "temp_block_id", "additional_attributes"]
+        fields = ["id", "block_id", "type", "content", "note", "note_id", "position", "temp_block_id", "additional_attributes", "relative_position"]
         read_only_fields = ["id"]
         list_serializer_class = BlockListSerializer
         extra_kwargs = {
